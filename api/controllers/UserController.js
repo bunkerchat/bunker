63998b13766597d0c5f45aecd5048f59c5f1c09c
xjpro <jprodahl@gmail.com>
2014-10-01 10:12:46 -0500

    fixing issue where joined rooms would not show in header/lobby until refresh

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -5,12 +5,12 @@
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
-module.exports = {
-	current: function(req, res) {
-		User.findOne(req.session.user.id).populateAll().exec(function(error, user) {
-			User.subscribe(req.socket, user, ['update']);
-			res.json(user);
-		});
-	}
-};
+var actionUtil = require('../../node_modules/sails/lib/hooks/blueprints/actionUtil');
 
+module.exports.current = function (req, res) {
+	User.findOne(req.session.user.id).populateAll().exec(function (error, user) {
+		User.subscribe(req, user);
+		actionUtil.subscribeDeep(req, user);
+		res.json(user);
+	});
+};
--- assets/app/room/RoomController.js
+++ assets/app/room/RoomController.js
@@ -1,15 +1,17 @@
-app.controller('RoomController', function($state, $stateParams, bunkerApi, user) {
-	bunkerApi.room.get({id: $stateParams.roomId}, function(room) {
+app.controller('RoomController', function ($state, $stateParams, bunkerApi, user) {
+	bunkerApi.room.get({id: $stateParams.roomId}, function (room) {
 		var currentRoomMembers = room.members; // TODO combine with owners
-		if(!_.any(currentRoomMembers, {id: user.id})) { // user is not a current member
-			if(room.isPrivate) {
+		if (!_.any(currentRoomMembers, {id: user.id})) { // user is not a current member
+			if (room.isPrivate) {
 				// TODO prompt for code
 				$state.go('lobby');
 			}
 			else {
 				// join as a member
 				room.members.push(user.id);
-				room.$save();
+				room.$save(function () {
+					user.rooms.push(room);
+				});
 			}
 		}
 	});
