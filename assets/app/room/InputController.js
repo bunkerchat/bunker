db7c8d8a40e69ffe84374c0eb04318ca89e0da2d
xjpro <jprodahl@gmail.com>
2014-10-03 16:29:52 -0500

    chat history with arrow keys

--- assets/app/room/InputController.js
+++ assets/app/room/InputController.js
@@ -1,5 +1,8 @@
 app.controller('InputController', function ($stateParams, bunkerApi) {
 	this.messageText = '';
+	this.submittedMessages = [];
+	this.selectedMessageIndex = -1;
+
 	this.sendMessage = function () {
 		if (!this.messageText) return;
 
@@ -7,6 +10,23 @@ app.controller('InputController', function ($stateParams, bunkerApi) {
 		newMessage.room = $stateParams.roomId;
 		newMessage.text = this.messageText;
 		newMessage.$save();
+
+		this.submittedMessages.unshift(this.messageText);
+		this.selectedMessageIndex = -1;
 		this.messageText = '';
+	};
+	this.keyDown = function(evt) {
+		if(evt.keyCode == 38 || evt.keyCode == 40) { // up or down
+			this.selectedMessageIndex += evt.keyCode == 38 ? 1 : -1;
+
+			if(this.selectedMessageIndex < 0) {
+				this.selectedMessageIndex = 0;
+			}
+			else if(this.selectedMessageIndex >= this.submittedMessages.length) {
+				this.selectedMessageIndex = this.submittedMessages.length-1;
+			}
+
+			this.messageText = this.submittedMessages[this.selectedMessageIndex];
+		}
 	};
 });
