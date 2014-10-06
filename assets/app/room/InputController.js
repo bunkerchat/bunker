32de3b8969d0b7ecf50ecac848887bd9068ee456
xjpro <jprodahl@gmail.com>
2014-10-06 09:45:34 -0500

    support multiple emoticons in text; better resetting

--- assets/app/room/InputController.js
+++ assets/app/room/InputController.js
@@ -13,30 +13,39 @@ app.controller('InputController', function ($stateParams, bunkerApi, emoticons) 
 		newMessage.text = this.messageText;
 		newMessage.$save();
 
+		// Save message for up/down keys to retrieve
 		this.submittedMessages.unshift(this.messageText);
+
+		// Reset all the things
 		this.selectedMessageIndex = -1;
 		this.messageText = '';
+		emoticonSearch = '';
+		emoticonSearchIndex = -1;
 	};
 	this.keyDown = function(evt) {
 		if (evt.keyCode == 9) { // tab
 			evt.preventDefault();
 
-			var matchingEmoticons = _.filter(emoticons.names, function (emoticon) {
-				return emoticon.indexOf(emoticonSearch) == 0
-			});
+			if(emoticonSearch) {
+				var matchingEmoticons = _.filter(emoticons.names, function (emoticon) {
+					return emoticon.indexOf(emoticonSearch) == 0
+				});
 
-			if(evt.shiftKey) {
-				emoticonSearchIndex = emoticonSearchIndex > 0 // TODO this is broken
-					? Math.max(emoticonSearchIndex - 1, 0)
-					: matchingEmoticons.length - 1;
+				if (evt.shiftKey) {
+					emoticonSearchIndex = emoticonSearchIndex > 0 // TODO this is broken
+						? Math.max(emoticonSearchIndex - 1, 0)
+						: matchingEmoticons.length - 1;
+				}
+				else {
+					emoticonSearchIndex = emoticonSearchIndex < matchingEmoticons.length - 1
+						? Math.min(emoticonSearchIndex + 1, matchingEmoticons.length - 1)
+						: 0;
+				}
+
+				this.messageText = this.messageText.replace(/:\w+:?$/, ':' + matchingEmoticons[emoticonSearchIndex] + ':');
+				//console.log(_.last(lastEmoticon));
+				//console.log(emoticonSearch, emoticonSearchIndex, matchingEmoticons);
 			}
-			else {
-				emoticonSearchIndex = emoticonSearchIndex < matchingEmoticons.length - 1
-					? Math.min(emoticonSearchIndex + 1, matchingEmoticons.length - 1)
-					: 0;
-			}
-			this.messageText = ':' + matchingEmoticons[emoticonSearchIndex] + ':';
-			//console.log(emoticonSearch, emoticonSearchIndex, matchingEmoticons);
 		}
 	};
 	this.keyUp = function (evt) {
@@ -52,11 +61,11 @@ app.controller('InputController', function ($stateParams, bunkerApi, emoticons) 
 
 			this.messageText = this.submittedMessages[this.selectedMessageIndex];
 		}
-		else if (/^:\w+$/.test(this.messageText) && evt.keyCode != 9 && evt.keyCode != 16) {
+		else if (/:\w+$/.test(this.messageText) && evt.keyCode != 9 && evt.keyCode != 16) {
 			//console.log('txt is', this.messageText, evt.keyCode);
-			emoticonSearch = this.messageText.substring(1, this.messageText.length);
+			emoticonSearch = this.messageText.match(/:(\w+)$/)[1];
 			emoticonSearchIndex = -1;
-			//console.log(emoticonSearch)
+			//console.log(emoticonSearch);
 		}
 		//console.log(evt.keyCode, evt);
 	};
