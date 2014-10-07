f53a7a2a28460fafab7fce7436e386d94e931ad5
Jason More <Jason.More@gmail.com>
2014-10-07 09:18:06 -0500

    putting user sessions in a seperate database to *hopefully* fix mongo TTL

--- config/session.js
+++ config/session.js
@@ -12,83 +12,83 @@
  * http://sailsjs.org/#/documentation/reference/sails.config/sails.config.session.html
  */
 
-var mongoUrl =process.env.MONGO_URL || process.env.MONGOHQ_URL || 'mongodb://localhost:27017/bunker';
+var mongoUrl = process.env.MONGO_URL_SESSION || process.env.MONGOLAB_URI || 'mongodb://localhost:27017/bunker_sessions';
 
 module.exports.session = {
 
-  /***************************************************************************
-  *                                                                          *
-  * Session secret is automatically generated when your new app is created   *
-  * Replace at your own risk in production-- you will invalidate the cookies *
-  * of your users, forcing them to log in again.                             *
-  *                                                                          *
-  ***************************************************************************/
-  secret: '64ec1dff67add7c8ff0b08e0b518e43c',
+	/***************************************************************************
+	 *                                                                          *
+	 * Session secret is automatically generated when your new app is created   *
+	 * Replace at your own risk in production-- you will invalidate the cookies *
+	 * of your users, forcing them to log in again.                             *
+	 *                                                                          *
+	 ***************************************************************************/
+	secret: '64ec1dff67add7c8ff0b08e0b518e43c',
 
 
-  /***************************************************************************
-  *                                                                          *
-  * Set the session cookie expire time The maxAge is set by milliseconds,    *
-  * the example below is for 24 hours                                        *
-  *                                                                          *
-  ***************************************************************************/
+	/***************************************************************************
+	 *                                                                          *
+	 * Set the session cookie expire time The maxAge is set by milliseconds,    *
+	 * the example below is for 24 hours                                        *
+	 *                                                                          *
+	 ***************************************************************************/
 
-  // cookie: {
-  //   maxAge: 24 * 60 * 60 * 1000
-  // }
+	// cookie: {
+	//   maxAge: 24 * 60 * 60 * 1000
+	// }
 
-  /***************************************************************************
-  *                                                                          *
-  * In production, uncomment the following lines to set up a shared redis    *
-  * session store that can be shared across multiple Sails.js servers        *
-  ***************************************************************************/
+	/***************************************************************************
+	 *                                                                          *
+	 * In production, uncomment the following lines to set up a shared redis    *
+	 * session store that can be shared across multiple Sails.js servers        *
+	 ***************************************************************************/
 
-  // adapter: 'redis',
+	// adapter: 'redis',
 
-  /***************************************************************************
-  *                                                                          *
-  * The following values are optional, if no options are set a redis         *
-  * instance running on localhost is expected. Read more about options at:   *
-  * https://github.com/visionmedia/connect-redis                             *
-  *                                                                          *
-  *                                                                          *
-  ***************************************************************************/
+	/***************************************************************************
+	 *                                                                          *
+	 * The following values are optional, if no options are set a redis         *
+	 * instance running on localhost is expected. Read more about options at:   *
+	 * https://github.com/visionmedia/connect-redis                             *
+	 *                                                                          *
+	 *                                                                          *
+	 ***************************************************************************/
 
-  // host: 'localhost',
-  // port: 6379,
-  // ttl: <redis session TTL in seconds>,
-  // db: 0,
-  // pass: <redis auth password>
-  // prefix: 'sess:'
+	// host: 'localhost',
+	// port: 6379,
+	// ttl: <redis session TTL in seconds>,
+	// db: 0,
+	// pass: <redis auth password>
+	// prefix: 'sess:'
 
 
-  /***************************************************************************
-  *                                                                          *
-  * Uncomment the following lines to use your Mongo adapter as a session     *
-  * store                                                                    *
-  *                                                                          *
-  ***************************************************************************/
+	/***************************************************************************
+	 *                                                                          *
+	 * Uncomment the following lines to use your Mongo adapter as a session     *
+	 * store                                                                    *
+	 *                                                                          *
+	 ***************************************************************************/
 
-  adapter: 'mongo',
-	url:mongoUrl + '/bunker_user_sessions'
-  // host: 'localhost',
-  // port: 27017,
-  // db: 'sails',
-  // collection: 'sessions',
+	adapter: 'mongo',
+	url: mongoUrl + '/bunker_user_sessions'
+	// host: 'localhost',
+	// port: 27017,
+	// db: 'sails',
+	// collection: 'sessions',
 
-  /***************************************************************************
-  *                                                                          *
-  * Optional Values:                                                         *
-  *                                                                          *
-  * # Note: url will override other connection settings url:                 *
-  * 'mongodb://user:pass@host:port/database/collection',                     *
-  *                                                                          *
-  ***************************************************************************/
+	/***************************************************************************
+	 *                                                                          *
+	 * Optional Values:                                                         *
+	 *                                                                          *
+	 * # Note: url will override other connection settings url:                 *
+	 * 'mongodb://user:pass@host:port/database/collection',                     *
+	 *                                                                          *
+	 ***************************************************************************/
 
-  // username: '',
-  // password: '',
-  // auto_reconnect: false,
-  // ssl: false,
-  // stringify: true
+	// username: '',
+	// password: '',
+	// auto_reconnect: false,
+	// ssl: false,
+	// stringify: true
 
 };
