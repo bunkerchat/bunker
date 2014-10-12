26ebe01026f9c9eb611324861f52c1886aa50341
DigitalZebra <DigitalZebra@users.noreply.github.com>
2014-10-12 13:33:12 -0500

    working on getting outside menu click to close menu. has a bug

--- assets/app/header/emoticonMenu.js
+++ assets/app/header/emoticonMenu.js
@@ -1,11 +1,11 @@
-app.directive('emoticonMenu', function($rootScope, $filter) {
+app.directive('emoticonMenu', function($rootScope, $filter, $document) {
 	return {
 		templateUrl: '/assets/app/header/emoticonMenu.html',
 		scope: {
 			emoticons: '=emoticonMenu',
 			visible: '=ngShow'
 		},
-		link: function(scope) {
+		link: function(scope, el) {
 			scope.emoticonMenuLists = [
 				_.initial(scope.emoticons.files, Math.floor(scope.emoticons.files.length/2)),
 				_.rest(scope.emoticons.files, Math.ceil(scope.emoticons.files.length/2))
@@ -19,6 +19,19 @@ app.directive('emoticonMenu', function($rootScope, $filter) {
 					scope.visible = false;
 				}
 			};
+
+            $document.on('click.emoticons', function(e) {
+                var wasEmoticonMenuClicked = $(e.target).closest(el).length > 0;
+
+                if (!wasEmoticonMenuClicked && scope.visible) {
+                    scope.visible = false;
+                    scope.$apply();
+                }
+            });
+
+            scope.$on('$destroy', function() {
+               $document.off('click.emoticons');
+            });
 		}
 	}
 });
