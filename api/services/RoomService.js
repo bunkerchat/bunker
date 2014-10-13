65a244390369a23ae233f1e5d89abb74f85d2e35
xjpro <jprodahl@gmail.com>
2014-10-13 11:14:10 -0500

    refactoring; adjusting styling on alert messages borders

--- api/services/RoomService.js
+++ api/services/RoomService.js
@@ -1,10 +1,22 @@
 
-module.exports.updateAllWithUser = function(userId) {
+var uuid = require('node-uuid');
+
+module.exports.updateAllWithUser = function(userId, systemMessage) {
 	Room.find().populate('members').exec(function (error, rooms) {
 		if (error) return false;
 		_.each(rooms, function (room) {
 			if (_.any(room.members, {id: userId})) {
 				Room.publishUpdate(room.id, room);
+
+				// If we were provided a message, send it down to affected rooms
+				if(systemMessage) {
+					Room.message(room.id, {
+						id: uuid.v4(),
+						text: systemMessage,
+						room: room.id,
+						createdAt: new Date().toISOString()
+					});
+				}
 			}
 		});
 		return true;
