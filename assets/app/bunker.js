fe2f032b06cebff73b44aec341257b2b2e6be1b2
DigitalZebra <DigitalZebra@users.noreply.github.com>
2014-10-25 23:43:24 -0500

    ugh fixing tabs

--- assets/app/bunker.js
+++ assets/app/bunker.js
@@ -1,48 +1,48 @@
 window.app = angular.module('bunker', [
-    'ngSanitize',
-    'sailsResource',
-    'ui.router',
-    'ui.gravatar',
-    'angularMoment',
-    'ngResource',
-    'ui.bootstrap'
+	'ngSanitize',
+	'sailsResource',
+	'ui.router',
+	'ui.gravatar',
+	'angularMoment',
+	'ngResource',
+	'ui.bootstrap'
 ])
-    .config(function ($stateProvider, $urlRouterProvider) {
-        $urlRouterProvider.otherwise('/');
-        $stateProvider
-            .state('lobby', {
-                url: '/',
-                templateUrl: '/assets/app/lobby/lobby.html',
-                controller: 'LobbyController as lobby'
-            })
-            .state('room', {
-                url: '/rooms/{roomId}',
-                templateUrl: '/assets/app/room/room.html',
-                controller: 'RoomController as room',
-                resolve: {
-                    currentRoom: function ($stateParams, rooms) {
-                        // Angular UI router will complete this before creating the controller if a $promise is returned
-                        return rooms($stateParams.roomId).$promise;
-                    }
-                }
-            })
-            .state('roomHistory', {
-                url: '/rooms/{roomId}/history?date',
-                templateUrl: '/assets/app/room/roomHistory.html',
-                controller: 'RoomHistoryController as room',
-                resolve: {
-                    currentUser: function (user) {
-                        return user.current.$promise;
-                    }
-                }
-            });
-    })
-    .config(function ($compileProvider) {
-        // This is where we might customize the sanitize whitelist some day
-        // $compileProvider.imgSrcSanitizationWhitelist();
-    })
-    .config(function (gravatarServiceProvider) {
-        gravatarServiceProvider.defaults = {
-            'default': 'identicon'
-        };
-    });
+	.config(function ($stateProvider, $urlRouterProvider) {
+		$urlRouterProvider.otherwise('/');
+		$stateProvider
+			.state('lobby', {
+				url: '/',
+				templateUrl: '/assets/app/lobby/lobby.html',
+				controller: 'LobbyController as lobby'
+			})
+			.state('room', {
+				url: '/rooms/{roomId}',
+				templateUrl: '/assets/app/room/room.html',
+				controller: 'RoomController as room',
+				resolve: {
+					currentRoom: function ($stateParams, rooms) {
+						// Angular UI router will complete this before creating the controller if a $promise is returned
+						return rooms($stateParams.roomId).$promise;
+					}
+				}
+			})
+			.state('roomHistory', {
+				url: '/rooms/{roomId}/history?date',
+				templateUrl: '/assets/app/room/roomHistory.html',
+				controller: 'RoomHistoryController as room',
+				resolve: {
+					currentUser: function (user) {
+						return user.current.$promise;
+					}
+				}
+			});
+	})
+	.config(function ($compileProvider) {
+		// This is where we might customize the sanitize whitelist some day
+		// $compileProvider.imgSrcSanitizationWhitelist();
+	})
+	.config(function (gravatarServiceProvider) {
+		gravatarServiceProvider.defaults = {
+			'default': 'identicon'
+		};
+	});
--- assets/app/room/fileEvents/fileEvents.js
+++ assets/app/room/fileEvents/fileEvents.js
@@ -1,60 +1,59 @@
-
 angular
-    .module('fileEvents', [])
+	.module('fileEvents', [])
 
-    .factory('imageUpload', function($http) {
+	.factory('imageUpload', function ($http) {
 
-        return {
-            doUpload: function(base64ImageData, progress) {
+		return {
+			doUpload: function (base64ImageData, progress) {
 
-                var xmlHttpRequest = new XMLHttpRequest();
-                xmlHttpRequest.open('POST', 'https://api.imgur.com/3/image');
+				var xmlHttpRequest = new XMLHttpRequest();
+				xmlHttpRequest.open('POST', 'https://api.imgur.com/3/image');
 
-                xmlHttpRequest.setRequestHeader('Authorization', 'Client-ID ' + 'f9b49a92d8ec31b');
-                xmlHttpRequest.setRequestHeader('Accept', 'application/json');
+				xmlHttpRequest.setRequestHeader('Authorization', 'Client-ID ' + 'f9b49a92d8ec31b');
+				xmlHttpRequest.setRequestHeader('Accept', 'application/json');
 
-                xmlHttpRequest.onload = function(e) {
-                    $('#finalImage').val(JSON.parse(e.target.responseText).data.link);
-                };
+				xmlHttpRequest.onload = function (e) {
+					$('#finalImage').val(JSON.parse(e.target.responseText).data.link);
+				};
 
-                xmlHttpRequest.onerror = function(e) {
-                    debugger;
-                };
+				xmlHttpRequest.onerror = function (e) {
+					debugger;
+				};
 
-                var formData = new FormData();
-                formData.append('type', 'base64');
-                formData.append('image', base64ImageData);
+				var formData = new FormData();
+				formData.append('type', 'base64');
+				formData.append('image', base64ImageData);
 
-                xmlHttpRequest.send(formData);
+				xmlHttpRequest.send(formData);
 
-                // jQuery codes
-                //
-                // $.ajax({
-                //   xhr: function() {
-                //     var xhr = new XMLHttpRequest();
+				// jQuery codes
+				//
+				// $.ajax({
+				//   xhr: function() {
+				//     var xhr = new XMLHttpRequest();
 
-                //     xhr.upload.addEventListener('progress', handleProgress, false);
-                //     // xhr.addEventListener('progress', handleProgress, false); // only required for download
+				//     xhr.upload.addEventListener('progress', handleProgress, false);
+				//     // xhr.addEventListener('progress', handleProgress, false); // only required for download
 
-                //     return xhr;
-                //   },
-                //   url: 'https://api.imgur.com/3/image',
-                //   type: 'POST',
-                //   headers: {
-                //     Authorization: 'Client-ID ' + 'f9b49a92d8ec31b',
-                //     Accept: 'application/json'
-                //   },
-                //   data: {
-                //     image: datas,
-                //     type: 'base64'
-                //   },
-                //   success: function(result) {
-                //     $('#finalImage').val(result.data.link);
-                //     console.log(result);
-                //   }
-                // });
-            }
-        };
+				//     return xhr;
+				//   },
+				//   url: 'https://api.imgur.com/3/image',
+				//   type: 'POST',
+				//   headers: {
+				//     Authorization: 'Client-ID ' + 'f9b49a92d8ec31b',
+				//     Accept: 'application/json'
+				//   },
+				//   data: {
+				//     image: datas,
+				//     type: 'base64'
+				//   },
+				//   success: function(result) {
+				//     $('#finalImage').val(result.data.link);
+				//     console.log(result);
+				//   }
+				// });
+			}
+		};
 
-    });
+	});
 
