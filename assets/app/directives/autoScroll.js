248b29e670d49280ff00ccd5ada8245be339cd3f
Jason More <Jason.More@gmail.com>
2014-10-31 10:32:24 -0500

    made autoscroll work better with images and code cleanup

--- assets/app/directives/autoScroll.js
+++ assets/app/directives/autoScroll.js
@@ -6,20 +6,35 @@ app.directive('autoScroll', function ($location, $anchorScroll, $timeout) {
 		link: function (scope, elem) {
 			var el = angular.element(elem);
 			var firstTime = true;
-			scope.$watch('messageId', function (newVal, oldVal) {
-				if(newVal == oldVal) return;
+			scope.$watch('messageId', function (messageId, lastMessageId) {
+				if (messageId == lastMessageId) return;
 
 				// TODO why does height need a 1px tolerance?
 				var currentScroll = el.prop('scrollHeight') - el.prop('scrollTop');
-				if(currentScroll == el.height() || currentScroll == el.height() + 1) {
-						// scroll to message
-						$timeout(function() {
-							$location.hash(newVal);
-							$anchorScroll();
-						},firstTime ? 500 : 0);
+				var shouldScroll = currentScroll == el.height() || currentScroll == el.height() + 1;
+				if (!shouldScroll) return;
 
+				if(firstTime) {
+					$timeout(scroll, 500);
 					firstTime = false;
+					return;
+				}
+
+				$timeout(function () {
+					var image = angular.element('#' + messageId).find('img');
+
+					if (image.length) {
+						image.on('load', scroll);
+					} else {
+						scroll();
+					}
+				});
+
+				function scroll() {
+					$location.hash(messageId);
+					$anchorScroll();
 				}
 			});
-		}};
+		}
+	};
 });
