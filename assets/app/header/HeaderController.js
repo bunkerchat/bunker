693e2591ee16c1160aa7e1d92d3f8ae1676103e2
xjpro <jprodahl@gmail.com>
2014-11-04 15:57:52 -0600

    unread message badges

--- assets/app/header/HeaderController.js
+++ assets/app/header/HeaderController.js
@@ -1,9 +1,9 @@
-app.controller('HeaderController', function ($stateParams, $state, user, rooms, emoticons) {
+app.controller('HeaderController', function ($rootScope, $stateParams, $state, user, rooms, emoticons) {
 	var self = this;
 	this.user = user.current;
 	this.settings = user.settings;
 	this.emoticons = emoticons;
-	this.showOptions = function() {
+	this.showOptions = function () {
 		return !$state.is('lobby');
 	};
 
@@ -11,10 +11,26 @@ app.controller('HeaderController', function ($stateParams, $state, user, rooms, 
 		self.settings[setting] = !self.settings[setting];
 		self.settings.$save();
 	};
-	this.leaveRoom = function() {
+	this.leaveRoom = function () {
 		var room = rooms($stateParams.roomId);
-		room.$leave(function() {
+		room.$leave(function () {
 			$state.go('lobby');
 		});
 	};
+
+	$rootScope.$on('$sailsResourceMessaged', function (evt, resource) {
+		if (resource.model == 'room' && resource.data.author && !resource.data.edited) {
+			var otherRoom = _.find(user.current.rooms, {id: resource.id});
+			if (resource.id != $stateParams.roomId) {
+				otherRoom.$unreadMessages = otherRoom.$unreadMessages ? otherRoom.$unreadMessages + 1 : 1;
+			}
+		}
+	});
+
+	$rootScope.$on('$stateChangeSuccess', function() {
+		if($stateParams.roomId) {
+			var currentRoom = _.find(user.current.rooms, {id: $stateParams.roomId});
+			currentRoom.$unreadMessages = 0;
+		}
+	});
 });
