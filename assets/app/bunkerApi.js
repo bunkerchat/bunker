368ae3da2d1eaeb29f6e1e7719ff6103b190b106
Joshua Prodahl <jprodahl@gmail.com>
2014-11-12 13:09:06 -0600

    setting refetch after reconnect for latest messages

--- assets/app/bunkerApi.js
+++ assets/app/bunkerApi.js
@@ -6,14 +6,13 @@ app.factory('bunkerApi', function (sailsResource) {
 		userSettings: sailsResource('userSettings'),
 		roomMember: sailsResource('roomMember'),
 		message: sailsResource('message', {
-			query: {method: 'GET', isArray: true, cache: false, fetchAfterReconnect: true},
-			update: {method: 'PUT', isArray: false, cache: false, fetchAfterReconnect: false},
+			update: {method: 'PUT', isArray: false, cache: false},
 			emoticonCounts: {method: 'GET', isArray: true, cache: false, url: '/message/emoticonCounts'}
 		}),
 		room: sailsResource('room', {
 			get: {method: 'GET', fetchAfterReconnect: true},
 			leave: {method: 'PUT', url: '/room/leave'},
-			latest: {method: 'GET', isArray: true, cache: false, url: '/room/:roomId/latest'},
+			latest: {method: 'GET', isArray: true, cache: false, url: '/room/:roomId/latest', fetchAfterReconnect: true},
 			history: {method: 'GET', isArray: true, url: '/room/:roomId/history'}
 		})
 	};
