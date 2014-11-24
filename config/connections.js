e37569b0102841c10be468ace4d799488ebf7c82
Jason More <Jason.More@gmail.com>
2014-11-24 10:59:33 -0600

    removed unused connections

--- config/connections.js
+++ config/connections.js
@@ -28,9 +28,9 @@ module.exports.connections = {
   * Installed by default.                                                    *
   *                                                                          *
   ***************************************************************************/
-  localDiskDb: {
-    adapter: 'sails-disk'
-  },
+  //localDiskDb: {
+  //  adapter: 'sails-disk'
+  //},
 
   /***************************************************************************
   *                                                                          *
@@ -40,13 +40,13 @@ module.exports.connections = {
   * Run: npm install sails-mysql                                             *
   *                                                                          *
   ***************************************************************************/
-  someMysqlServer: {
-    adapter: 'sails-mysql',
-    host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-    user: 'YOUR_MYSQL_USER',
-    password: 'YOUR_MYSQL_PASSWORD',
-    database: 'YOUR_MYSQL_DB'
-  },
+  //someMysqlServer: {
+  //  adapter: 'sails-mysql',
+  //  host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
+  //  user: 'YOUR_MYSQL_USER',
+  //  password: 'YOUR_MYSQL_PASSWORD',
+  //  database: 'YOUR_MYSQL_DB'
+  //},
 
   /***************************************************************************
   *                                                                          *
@@ -59,7 +59,7 @@ module.exports.connections = {
   mongo: {
     adapter: 'sails-mongo',
     url: process.env.MONGO_URL || process.env.MONGOHQ_URL || 'mongodb://localhost:27017/bunker?auto_reconnect=true'
-  },
+  }
 
   /***************************************************************************
   *                                                                          *
@@ -70,13 +70,13 @@ module.exports.connections = {
   *                                                                          *
   *                                                                          *
   ***************************************************************************/
-  somePostgresqlServer: {
-    adapter: 'sails-postgresql',
-    host: 'YOUR_POSTGRES_SERVER_HOSTNAME_OR_IP_ADDRESS',
-    user: 'YOUR_POSTGRES_USER',
-    password: 'YOUR_POSTGRES_PASSWORD',
-    database: 'YOUR_POSTGRES_DB'
-  }
+  //somePostgresqlServer: {
+  //  adapter: 'sails-postgresql',
+  //  host: 'YOUR_POSTGRES_SERVER_HOSTNAME_OR_IP_ADDRESS',
+  //  user: 'YOUR_POSTGRES_USER',
+  //  password: 'YOUR_POSTGRES_PASSWORD',
+  //  database: 'YOUR_POSTGRES_DB'
+  //}
 
 
   /***************************************************************************
