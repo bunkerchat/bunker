2d46c9ef9321044fac99f0879a1fed6163de7b50
Jason More <Jason.More@gmail.com>
2015-01-22 09:45:57 -0600

    refactored nasty bunkerMessage link function

--- assets/app/messages/bunkerMessage.js
+++ assets/app/messages/bunkerMessage.js
@@ -7,16 +7,6 @@ app.directive('bunkerMessage', function ($compile, emoticons) {
 		return str.split(find).join(replace);
 	}
 
-	//function extractVideoID(url){
-	//	var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
-	//	var match = url.match(regExp);
-	//	if ( match && match[7].length == 11 ){
-	//		return match[7];
-	//	}else{
-	//		alert("Could not extract video ID.");
-	//	}
-	//}
-
 	return {
 		template: '<span ng-bind-html="formatted"></span>',
 		scope: {
@@ -24,7 +14,6 @@ app.directive('bunkerMessage', function ($compile, emoticons) {
 			media: '@'
 		},
 		link: function (scope, elem) {
-			var parseMedia = typeof scope.media !== 'undefined' ? scope.$eval(scope.media) : true;
 
 			// since we are passing in a bunker message OR room, run the watch on the room topic
 			scope.$watch('bunkerMessage.text', textWatch);
@@ -33,96 +22,114 @@ app.directive('bunkerMessage', function ($compile, emoticons) {
 			function textWatch(text) {
 				if (!text) return;
 
-				var formatted = text,
-					replacedEmotes = {},
-					replacedLinks = {};
-
 				// Parse quotes
 				if (text.match(/\n[^$]/g)) {
-
-					// Scan for overtabs
-					var lines = formatted.split('\n');
-					var firstLineSpacingMatch = _.first(lines).match(/^(\s+)/);
-
-					// If we have some spacing on the first line, remove the same spacing from all other lines (detabs everything)
-					if (firstLineSpacingMatch) {
-						var firstLineSpacingExpression = new RegExp('^' + firstLineSpacingMatch[1] + '');
-						var spacingRemoved = _.map(lines, function (line) {
-							return line.replace(firstLineSpacingExpression, '');
-						});
-						formatted = spacingRemoved.join('\n');
-					}
-
-					// Put in code block
-					var quote = [];
-					quote.push('<div class="panel"><pre>');
-					quote.push(formatted);
-					quote.push('</pre></div>');
-
-					formatted = quote.join('');
+					text = createQuotedBlock(text);
 				}
 				else {
-
-					// Parse bold
-					_.each(text.match(/(?:[^A-Za-z0-9]|^)(\*[^\*]+\*)(?:[^A-Za-z0-9]|$)/g), function (bold) {
-						formatted = replaceAll(formatted, bold, '<strong>' + bold.replace(/\*/g, '') + '</strong>');
-					});
-
-					// Parse italics
-					_.each(text.match(/(?:[^A-Za-z0-9]|^)(_[^_]+_)(?:[^A-Za-z0-9]|$)/g), function (italics) {
-						formatted = replaceAll(formatted, italics, '<em>' + italics.replace(/_/g, '') + '</em>');
-					});
-
-					// Parse emoticons
-					_.each(text.match(/:\w+:/g), function (emoticonText) {
-						var knownEmoticon = _.find(emoticons.files, function (known) {
-							return known.replace(/\.\w{1,4}$/, '').toLowerCase() == emoticonText.replace(/:/g, '').toLowerCase();
-						});
-						if (knownEmoticon && !replacedEmotes[knownEmoticon]) {
-							formatted = replaceAll(formatted, emoticonText,
-								'<img class="emoticon" title="' + emoticonText + '" src="/assets/images/emoticons/' + knownEmoticon + '"/>');
-							replacedEmotes[knownEmoticon] = true;
-						}
-					});
-
-					// Parse links
-					var attachedMedia;
-					_.each(text.match(/https?:\/\/\S+/gi), function (link) {
-
-						// Only parse media (images, youtube) if asked to
-						if (parseMedia) {
-							if (/\.(gif|png|jpg|jpeg)$/i.test(link) && !attachedMedia) {
-								// Image link
-								attachedMedia = angular.element('<div message="bunkerMessage" bunker-media="' + link + '"><img src="' + link + '"/></div>');
-							}
-							else if (/\.(gifv|mp4)$/i.test(link) && !attachedMedia) {
-								attachedMedia = angular.element('' +
-								'<div bunker-media="' + link + '">' +
-								'<video autoplay loop muted><source type="video/mp4" src="' + link.toLowerCase().replace('gifv', 'mp4') + '"></video>' +
-								'</div>');
-							}
-							else if (/(www\.)?(youtube\.com|youtu\.?be)\/watch/i.test(link) && !attachedMedia) {
-								attachedMedia = angular.element('' +
-								'<div class="default-video-height" message="bunkerMessage" bunker-media="' + link + '">' +
-								'<youtube-video video-url="\'' + link + '\'"></youtube-video>' +
-								'</div>');
-							}
-						}
-
-						if (!replacedLinks[link]) {
-							formatted = replaceAll(formatted, link, '<a href="' + link + '" target="_blank">' + link + '</a>');
-							replacedLinks[link] = true;
-						}
-					});
-
-					// If we made an image, attach it now
-					if (attachedMedia) {
-						angular.element(elem).append(attachedMedia);
-						$compile(attachedMedia)(scope.$new());
-					}
+					text = parseBoldAndItalics(text);
+					text = parseEmoticons(text);
+					text = parseMedia(text);
 				}
 
-				scope.formatted = formatted;
+				scope.formatted = text;
+			}
+
+			function createQuotedBlock(text) {
+				// Scan for overtabs
+				var lines = text.split('\n');
+				var firstLineSpacingMatch = _.first(lines).match(/^(\s+)/);
+
+				// If we have some spacing on the first line, remove the same spacing from all other lines (detabs everything)
+				if (firstLineSpacingMatch) {
+					var firstLineSpacingExpression = new RegExp('^' + firstLineSpacingMatch[1] + '');
+					var spacingRemoved = _.map(lines, function (line) {
+						return line.replace(firstLineSpacingExpression, '');
+					});
+					text = spacingRemoved.join('\n');
+				}
+
+				// Put in code block
+				var quote = [];
+				quote.push('<div class="panel"><pre>');
+				quote.push(text);
+				quote.push('</pre></div>');
+
+				return quote.join('');
+			}
+
+			function parseEmoticons(text) {
+				var replacedEmotes = {};
+
+				// Parse emoticons
+				_.each(text.match(/:\w+:/g), function (emoticonText) {
+					var knownEmoticon = _.find(emoticons.files, function (known) {
+						return known.replace(/\.\w{1,4}$/, '').toLowerCase() == emoticonText.replace(/:/g, '').toLowerCase();
+					});
+					if (knownEmoticon && !replacedEmotes[knownEmoticon]) {
+						text = replaceAll(text, emoticonText,
+							'<img class="emoticon" title="' + emoticonText + '" src="/assets/images/emoticons/' + knownEmoticon + '"/>');
+						replacedEmotes[knownEmoticon] = true;
+					}
+				});
+				return text;
+			}
+
+			function parseBoldAndItalics(text) {
+				// Parse bold
+				_.each(text.match(/(?:[^A-Za-z0-9]|^)(\*[^\*]+\*)(?:[^A-Za-z0-9]|$)/g), function (bold) {
+					text = replaceAll(text, bold, '<strong>' + bold.replace(/\*/g, '') + '</strong>');
+				});
+
+				// Parse italics
+				_.each(text.match(/(?:[^A-Za-z0-9]|^)(_[^_]+_)(?:[^A-Za-z0-9]|$)/g), function (italics) {
+					text = replaceAll(text, italics, '<em>' + italics.replace(/_/g, '') + '</em>');
+				});
+
+				return text;
+			}
+
+			function parseMedia(text) {
+				var shouldParseMedia = typeof scope.media !== 'undefined' ? scope.$eval(scope.media) : true;
+				var replacedLinks = {};
+
+				// Parse links
+				var attachedMedia;
+				_.each(text.match(/https?:\/\/\S+/gi), function (link) {
+
+					// Only parse media (images, youtube) if asked to
+					if (shouldParseMedia) {
+						if (/\.(gif|png|jpg|jpeg)$/i.test(link) && !attachedMedia) {
+							// Image link
+							attachedMedia = angular.element('<div message="bunkerMessage" bunker-media="' + link + '"><img src="' + link + '"/></div>');
+						}
+						else if (/\.(gifv|mp4)$/i.test(link) && !attachedMedia) {
+							attachedMedia = angular.element('' +
+							'<div bunker-media="' + link + '">' +
+							'<video autoplay loop muted><source type="video/mp4" src="' + link.toLowerCase().replace('gifv', 'mp4') + '"></video>' +
+							'</div>');
+						}
+						else if (/(www\.)?(youtube\.com|youtu\.?be)\/watch/i.test(link) && !attachedMedia) {
+							attachedMedia = angular.element('' +
+							'<div class="default-video-height" message="bunkerMessage" bunker-media="' + link + '">' +
+							'<youtube-video video-url="\'' + link + '\'"></youtube-video>' +
+							'</div>');
+						}
+					}
+
+					if (!replacedLinks[link]) {
+						text = replaceAll(text, link, '<a href="' + link + '" target="_blank">' + link + '</a>');
+						replacedLinks[link] = true;
+					}
+				});
+
+				// If we made an image, attach it now
+				if (attachedMedia) {
+					angular.element(elem).append(attachedMedia);
+					$compile(attachedMedia)(scope.$new());
+				}
+
+				return text;
 			}
 		}
 	};
