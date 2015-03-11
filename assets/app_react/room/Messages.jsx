2ba907e7bf54b915823c05ba3bf54db04fd57117
Jason More <Jason.More@gmail.com>
2015-03-11 16:30:12 -0500

    woot show some message text

--- assets/app_react/room/Messages.jsx
+++ assets/app_react/room/Messages.jsx
@@ -1,6 +1,38 @@
 var Router = require('react-router');
 var RoomStore = require('./roomStore');
 
+var Message = React.createClass({
+	render() {
+		var message = this.props.message;
+		return (
+			<li className="message-container">
+				<div className="message">
+					<div>
+						<div className="message-author">
+							<span>
+								<div className="name">yay</div>
+								<span>x</span>
+							</span>
+						</div>
+						<div className="message-caret"></div>
+						<div className="message-body new-message-body">
+							<span>{message.text}</span>
+							<span className="message-info text-muted">
+								<i className="fa fa-pencil"></i>
+								<span>
+									<a className="text-muted">
+										<small>time</small>
+									</a>
+								</span>
+							</span>
+						</div>
+					</div>
+				</div>
+			</li>
+		)
+	}
+});
+
 var Messages = React.createClass({
 	mixins: [
 		Router.Navigation,
@@ -37,10 +69,26 @@ var Messages = React.createClass({
 
 	render() {
 		var room = this.state.room;
-		if(!room) return <div></div>;
+		if (!room) return <div></div>;
+
+		var messages = room.$messages.map(message => {
+			return (
+				<Message key={message.id} message={message}/>
+			)
+		});
 
 		return (
-			<div>yay room</div>
+			<div className="container-fluid">
+				<div className="row">
+					<div className="col-md-10 col-xs-12 no-gutter">
+						<div>
+							<ol className="list-unstyled message-list">
+								{messages}
+							</ol>
+						</div>
+					</div>
+				</div>
+			</div>
 		);
 	}
 });
