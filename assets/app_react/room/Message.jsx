6b9f0ce132266f6b1803f1692f323927972304ad
Jason More <Jason.More@gmail.com>
2015-03-17 10:16:02 -0500

    first in series checks

--- assets/app_react/room/Message.jsx
+++ assets/app_react/room/Message.jsx
@@ -1,21 +1,25 @@
 var Gravatar = require('./../components/Gravatar');
+var cx = require('classnames');
 
 var Message = React.createClass({
 	render() {
 		var message = this.props.message;
+
+		var messageClass = cx({
+			'message-body': true,
+			'new-message-body': message.$firstInSeries,
+			'edited': message.edited
+		});
+
 		return (
 			<li className="message-container">
 				<div className="message">
 					<div>
 						<div className="message-author">
-							<span>
-								<Gravatar email={message.author.email} size="20"/>
-								<div className="name">{message.author.nick}</div>
-								<span>x</span>
-							</span>
+							{this.authorInfo(message)}
 						</div>
-						<div className="message-caret"></div>
-						<div className="message-body new-message-body">
+							{this.caret(message)}
+						<div className={messageClass}>
 							<span>{message.text}</span>
 							<span className="message-info text-muted">
 								<i className="fa fa-pencil"></i>
@@ -29,6 +33,24 @@ var Message = React.createClass({
 					</div>
 				</div>
 			</li>
+		)
+	},
+
+	authorInfo(message) {
+		if (!message.$firstInSeries)return;
+		return (
+			<span>
+				<Gravatar email={message.author.email} size={20} default="identicon"/>
+				<div className="name">{message.author.nick}</div>
+				<span>x</span>
+			</span>
+		)
+	},
+
+	caret(message) {
+		if (!message.$firstInSeries)return;
+		return (
+			<div className="message-caret"></div>
 		)
 	}
 });
--- assets/app_react/stores/roomStore.js
+++ assets/app_react/stores/roomStore.js
@@ -1,3 +1,4 @@
+var uuid = require('node-uuid');
 var UserActions = require('../user/userActions');
 var MembershipStore = require('./membershipStore');
 
@@ -5,6 +6,7 @@ var RoomStore = Reflux.createStore({
 	listenables: [UserActions],
 
 	rooms: {},
+	messageLookup: {},
 
 	getState() {
 		return {
@@ -18,6 +20,8 @@ var RoomStore = Reflux.createStore({
 				io.socket.get(`/room/${membership.room.id}/join`, (room, JWR) => {
 					io.socket.get(`/room/${room.id}/messages`, (messages, JWR) => {
 						room.$messages = messages.reverse();
+						this.decorateMessages(room.$messages);
+
 						this.rooms[room.id] = room;
 						this.trigger(this.rooms);
 					});
@@ -26,15 +30,39 @@ var RoomStore = Reflux.createStore({
 		});
 	},
 
+	decorateMessages(messages) {
+		_.each(messages, function (message, index) {
+			var lastMessage = index > 0 && index < messages.length ? messages[index - 1] : null;
+			message.$firstInSeries = !lastMessage || !lastMessage.author || !message.author || lastMessage.author.id != message.author.id;
+		});
+	},
+
+	addMessage(roomId, message) {
+		if (!message.id) {
+			message.id = uuid.v4(); // All messages need ids
+		}
+
+		var room = rooms[roomId];
+		if (!room) throw new Error('Received message from a room we are not a member of!');
+		if (this.messageLookup[roomId][message.id]) return; // Message already exists!
+		//if (!user.settings.showNotifications && !message.author) return; // User does not want to see notifications
+
+		var lastMessage = _.last(room.$messages);
+		message.$firstInSeries = !lastMessage || !lastMessage.author || !message.author || lastMessage.author.id != message.author.id;
+
+		room.$messages.push(message);
+		this.messageLookup[roomId][message.id] = true; // Store messages in lookup object, this allows us to check for duplicates quickly
+	},
+
 	// actions
 
 	onSendMessage(roomId, message) {
 		io.socket.post('/message', {"room": roomId, "text": message}, (message, JWR) => {
-			if(JWR.statusCode !== 200){
-				//UserActions.sendMessage.failed(message);
+			if (JWR.statusCode !== 200) {
+				throw new Error(JWR);
 			}
-			//UserActions.sendMessage.completed(message);
-			//this.
+			this.addMessage(roomId, message);
+			this.trigger(this.rooms);
 		});
 	}
 });
