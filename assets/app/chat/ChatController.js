082e812b666a38f9bf9c7d7da200fd3da27498c8
Joshua Prodahl <jprodahl@gmail.com>
2015-03-25 13:24:54 -0500

    chat controller controls when room is shown

--- assets/app/chat/ChatController.js
+++ assets/app/chat/ChatController.js
@@ -1,10 +1,18 @@
-app.controller('ChatController', function ($rootScope, $stateParams, $state, user) {
-	this.memberships = user.memberships;
+app.controller('ChatController', function ($rootScope, $scope, $stateParams, $state, user) {
+	var self = this;
+
+	$scope.$watch(function() { return user.memberships; }, function() {
+		self.rooms = _.pluck(user.memberships, 'room');
+		if(self.rooms.length > 0) {
+			_.find(self.rooms, {id: $stateParams.roomId}).$selected = true;
+		}
+	}, true);
 
 	$rootScope.$on('$stateChangeStart', function (evt, toState, toParams, fromState) {
-		//if(toState.name === 'chat' && fromState.name === 'chat') {
-		//	//evt.preventDefault();
-		//}
-		//console.log(toState);
+		if(toState.name === 'chat' && fromState.name === 'chat') {
+			//evt.preventDefault();
+			//_.find(self.rooms, {id: toParams.roomId}).$selected = true;
+		}
+		console.log(toState);
 	});
 });
