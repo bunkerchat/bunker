b8730d1e94fa9465768aba4384ef5b7f26a4de18
Jason More <Jason.More@gmail.com>
2015-03-25 10:39:43 -0500

    better emoticon loading

--- api/controllers/ViewController.js
+++ api/controllers/ViewController.js
@@ -1,11 +1,22 @@
-module.exports = {
-	index: function (req, res) {
+var Promise = require('bluebird');
+var fs = Promise.promisifyAll(require('fs'));
+var path = require('path');
+
+module.exports.index = function (req, res) {
+	getEmoticonNamesFromDisk().then(function (emoticons) {
 		res.view('index', {
 			userId: req.session.userId,
-			isProduction: sails.config.environment === 'production'
+			isProduction: sails.config.environment === 'production',
+			emoticons: emoticons
 		});
-	},
-	login: function (req, res) {
-		res.view('login');
-	}
+	})
 };
+
+module.exports.login = function (req, res) {
+	res.view('login');
+};
+
+
+function getEmoticonNamesFromDisk() {
+	return fs.readdirAsync(path.join(__dirname, '..', '..','assets', 'images', 'emoticons'));
+}
