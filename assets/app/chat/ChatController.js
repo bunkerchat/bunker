b59d6e9ad312583e6a9f0618c6bec1cd4f9f2b9a
Joshua Prodahl <jprodahl@gmail.com>
2015-03-27 09:03:50 -0500

    don't push a blank room unless rooms have come back

--- assets/app/chat/ChatController.js
+++ assets/app/chat/ChatController.js
@@ -8,7 +8,7 @@ app.controller('ChatController', function ($rootScope, $scope, $stateParams, $st
 
 	function selectRoom() {
 
-		if (!_.any(self.rooms, {id: $rootScope.roomId})) {
+		if (bunkerData.$resolved && !_.any(self.rooms, {id: $rootScope.roomId})) {
 			// Functionality to allow users to join a room by entering it's URL
 			self.rooms.push({id: $rootScope.roomId})
 		}
