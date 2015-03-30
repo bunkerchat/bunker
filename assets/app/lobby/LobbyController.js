8e03846e97c874f6ce005def2e506d9cce7a1fb6
Jason More <jasonmore@Jasons-MacBook-Pro-2.local>
2015-03-30 13:45:58 -0500

    refactored lobby

--- assets/app/lobby/LobbyController.js
+++ assets/app/lobby/LobbyController.js
@@ -1,21 +1,7 @@
-app.controller('LobbyController', function ($state, bunkerApi, rooms, user) {
+app.controller('LobbyController', function ($state, bunkerApi, bunkerData, rooms, user) {
 	var self = this;
 
-	// TODO this shows a room after it's been left :I
-	user.memberships.$promise.then(function (memberships) {
-		self.rooms = [];
-		// Get all known rooms and populate with member counts
-
-		_(memberships).pluck('room').each(function (room) {
-			bunkerApi.roomMember.query({room: room.id}, function (roomMembers) {
-				room.$memberCount = roomMembers.length;
-				room.$onlineMemberCount = _.filter(roomMembers, function (roomMember) {
-					return roomMember.user.connected;
-				}).length;
-				self.rooms.push(room);
-			});
-		});
-	});
+	this.bunkerData = bunkerData;
 
 	this.joinRoom = function (roomGuid) {
 		rooms.join(roomGuid).then(function (room) {
@@ -33,4 +19,14 @@ app.controller('LobbyController', function ($state, bunkerApi, rooms, user) {
 				// TODO show error
 			});
 	};
+});
+
+app.filter('connectedUsersCount', function () {
+	return function(members){
+		if(!members) return 0;
+
+		return members.filter(function (member) {
+			return member.connected;
+		}).length;
+	}
 });
