1d98b1f12a9c373fe3bcb4270a10c54d0ed1b558
Joshua Prodahl <jprodahl@gmail.com>
2015-04-08 14:57:39 -0500

    secure message update endpoint; clear typing and away flags on edit

--- api/controllers/MessageController.js
+++ api/controllers/MessageController.js
@@ -7,6 +7,7 @@
 
 var moment = require('moment');
 var actionUtil = require('../../node_modules/sails/lib/hooks/blueprints/actionUtil');
+var ForbiddenError = require('../errors/ForbiddenError');
 
 // PUT /message/:id
 // Update a message (the edit functionality)
@@ -14,16 +15,27 @@ exports.update = function (req, res) {
 
 	var messageEditWindowSeconds = 60;
 	var pk = actionUtil.requirePk(req);
+	var userId = req.session.userId;
 
-	Message.findOne(pk)
+	Message.findOne(pk).populate('author')
 		.then(function (message) {
 
+			if (userId != message.author.id) {
+				throw new ForbiddenError('Only the author may edit their message');
+			}
 			if (moment(message.createdAt).isBefore(moment().subtract(messageEditWindowSeconds, 'seconds'))) {
 				throw new Error('Edit window has past');
 			}
-			else if (message.edited) {
+			if (message.edited) {
 				throw new Error('Only one edit is allowed');
 			}
+
+			if (message.author.busy) {
+				// User is flagged as busy, we can now remove this flag since they are interacting with the app
+				User.update(userId, {busy: false}).exec();
+			}
+
+			User.publishUpdate(userId, {busy: false, typingIn: null});
 
 			var updates = { // Only certain things are editable
 				text: req.param('text'),
@@ -38,6 +50,9 @@ exports.update = function (req, res) {
 		})
 		.then(messageService.broadcastMessage)
 		.then(res.ok)
+		.catch(ForbiddenError, function (err) {
+			res.forbidden(err);
+		})
 		.catch(res.serverError);
 };
 
