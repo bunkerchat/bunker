49c33b6d7b6cce0f5b6847a4a22cc88ad07344a0
Joshua Prodahl <jprodahl@gmail.com>
2015-04-15 15:53:19 -0500

    fix message numbers not going down

--- assets/app/header/HeaderController.js
+++ assets/app/header/HeaderController.js
@@ -41,7 +41,7 @@ app.controller('HeaderController', function ($rootScope, $stateParams, $state, $
 		}
 	});
 
-	$rootScope.$on('roomIdChanged', function (roomId) {
+	$rootScope.$on('roomIdChanged', function (evt, roomId) {
 		var room = bunkerData.getRoom(roomId);
 		if (room) {
 			room.$unreadMessages = 0;
