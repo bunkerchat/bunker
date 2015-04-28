d055cca8818cca31743ab159a53c57dfabcea1f4
Jason More <Jason.More@gmail.com>
2015-04-28 17:04:46 -0500

    fixed emoticon sorting on app start

--- assets/app/header/emoticonMenu.js
+++ assets/app/header/emoticonMenu.js
@@ -1,29 +1,40 @@
-app.directive('emoticonMenu', function ($rootScope, $filter, emoticons, bunkerData) {
+app.directive('emoticonMenu', function ($rootScope, emoticons, bunkerData, fuzzyByFilter) {
 	return {
 		templateUrl: '/assets/app/header/emoticonMenu.html',
 		scope: {
 			visible: '=ngShow'
 		},
-		link: function ($scope) {
+		controllerAs: 'menu',
+		bindToController: true,
+		controller: function ($scope) {
+			var self = this;
+			this.search = '';
+			this.settings = bunkerData.userSettings;
+			this.saveSettings = bunkerData.saveUserSettings;
+			this.emoticonMenuLists = [];
 
-			$scope.search = '';
-			$scope.settings = bunkerData.userSettings;
-			$scope.saveSettings = bunkerData.saveUserSettings;
-			$scope.emoticonMenuLists = [];
+			refreshCounts();
 
 			function refreshCounts() {
 				bunkerData.getEmoticonCounts().then(function (emoticonCounts) {
-					$scope.emoticonCounts = emoticonCounts;
+					self.emoticonCounts = emoticonCounts;
+
+					var emoteCountsHash = _.indexBy(emoticonCounts, 'name');
+					_.each(emoticons.list, function (emoticon) {
+						emoticon.$count = emoteCountsHash[emoticon.name] ? emoteCountsHash[emoticon.name].count : 0;
+					});
+
+					prepareList();
 				});
 			}
 
 			function prepareList() {
 				var list = bunkerData.userSettings.sortEmoticonsByPopularity ? _(emoticons.list).sortBy('$count').reverse().value() : emoticons.list;
-				var filteredList = $filter('fuzzyBy')(list, 'name', $scope.search);
-				$scope.emoticonMenuLists = _.chunk(filteredList, 4);
+				var filteredList = fuzzyByFilter(list, 'name', self.search);
+				self.emoticonMenuLists = _.chunk(filteredList, 4);
 			}
 
-			$scope.$watch('settings.sortEmoticonsByPopularity', function (sortEmoticonsByPopularity, oldVal) {
+			$scope.$watch('menu.settings.sortEmoticonsByPopularity', function (sortEmoticonsByPopularity, oldVal) {
 				if (sortEmoticonsByPopularity == oldVal) return;
 				if (sortEmoticonsByPopularity) {
 					refreshCounts();
@@ -31,42 +42,32 @@ app.directive('emoticonMenu', function ($rootScope, $filter, emoticons, bunkerDa
 				prepareList();
 			});
 
-			$scope.$watch('visible', function (visible, oldVal) {
+			$scope.$watch('menu.visible', function (visible, oldVal) {
 				if (visible == oldVal) return;
 
 				if (!visible) {
-					$scope.search = '';
+					self.search = '';
 				}
-				else if (bunkerData.userSettings.sortEmoticonsByPopularity) {
+				if (bunkerData.userSettings.sortEmoticonsByPopularity) {
 					refreshCounts();
 				}
 			});
 
-			$scope.$watch('search', function () {
+			$scope.$watch('menu.search', function (search, oldVal) {
+				if (search == oldVal) return;
 				prepareList();
 			});
 
-			$scope.$watchCollection('emoticonCounts', function (emoteCounts, oldVal) {
-				if (emoteCounts == oldVal) return;
-
-				var emoteCountsHash = _.indexBy(emoteCounts, 'name');
-				_.each(emoticons.list, function (emoticon) {
-					emoticon.$count = emoteCountsHash[emoticon.name] ? emoteCountsHash[emoticon.name].count : 0;
-				});
-			});
-
-			$scope.appendEmoticonToChat = function (emoticonFileName) {
-				$scope.search = '';
+			this.appendEmoticonToChat = function (emoticonFileName) {
+				self.search = '';
 				var emoticonName = ':' + $filter('emoticonName')(emoticonFileName) + ':';
 				$rootScope.$broadcast('inputText', emoticonName);
 
 				// Close window
-				if (typeof $scope.visible !== 'undefined') {
-					$scope.visible = false;
+				if (typeof self.visible !== 'undefined') {
+					self.visible = false;
 				}
 			};
-
-			refreshCounts();
 		}
 	};
 });
