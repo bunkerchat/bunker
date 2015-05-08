f8e2b94dbf635f5823d513bcbba319e20adc28f0
Joshua Prodahl <jprodahl@gmail.com>
2015-05-08 17:26:30 -0500

    allow for multiple animations at once and cleanup

--- assets/app/room/room.js
+++ assets/app/room/room.js
@@ -1,10 +1,10 @@
-app.directive('room', function ($rootScope, bunkerData, $state) {
+app.directive('room', function ($rootScope, $state, bunkerData, emoticons) {
 	return {
 		scope: {
 			roomId: '@room'
 		},
 		templateUrl: '/assets/app/room/room.html',
-		link: function ($scope) {
+		link: function ($scope, $element) {
 
 			$scope.user = bunkerData.user;
 			$scope.settings = bunkerData.userSettings;
@@ -33,10 +33,39 @@ app.directive('room', function ($rootScope, bunkerData, $state) {
 			$scope.loadPreviousMessages = function () {
 				return bunkerData.loadMessages($scope.current, $scope.current.$messages.length);
 			};
+
+			$rootScope.$on('bunkerMessaged.animation', function (evt, message) {
+				if (message.room.id == $scope.current.id) {
+
+					var el = angular.element($element);
+					var emoticon = (/:\w+:/.exec(message.text))[0];
+					var knownEmoticon = _.find(emoticons.files, function (known) {
+						return known.replace(/\.\w{1,4}$/, '').toLowerCase() == emoticon.replace(/:/g, '').toLowerCase();
+					});
+
+					if (!knownEmoticon) return;
+
+					var animationBox = angular.element(
+						'<div class="animation-box closed" style="left: ' + (Math.random() * 60 + 20) + '%"><img src="/assets/images/emoticons/' + knownEmoticon + '"/></div>'
+					);
+
+					el.append(animationBox);
+					setTimeout(function () { // Put slide up on stack
+						animationBox.removeClass('closed');
+						setTimeout(function () { // Slide back down in 3 seconds
+							animationBox.addClass('closed');
+							setTimeout(function () { // Remove from DOM 1.5 seconds after requesting slide down
+								el.remove(animationBox);
+							}, 1500);
+						}, 3000);
+					});
+				}
+			});
+
 			$rootScope.$on('userUpdated', updateMemberList);
 
 			function updateMemberList() {
-				$scope.memberList =  _($scope.current.$members)
+				$scope.memberList = _($scope.current.$members)
 					.select(function (member) {
 						// Don't show users who haven't logged in for awhile
 						return moment().diff(member.user.lastConnected, 'days') < 45;
