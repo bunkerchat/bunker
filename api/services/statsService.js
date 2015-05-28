96c07a769e4b46a26ce6501b433795c1f8dd24c9
Joshua Prodahl <jprodahl@gmail.com>
2015-05-28 10:13:01 -0500

    most active day; total active days

--- api/services/statsService.js
+++ api/services/statsService.js
@@ -13,26 +13,64 @@ module.exports.getStats = function (roomMember) {
 				fs.readFileAsync(path.join(__dirname, 'statsTemplate.ejs')),
 				messageCount,
 				Message.count({author: roomMember.user.id, edited: true}),
+				getActiveDays(roomMember),
 				Message.find({author: roomMember.user.id}).sort('createdAt ASC').limit(1),
 				Message.find({author: roomMember.user.id}).skip(_.random(0, messageCount)).limit(1),
 				getEmoticonCounts(roomMember)
 			)
-				.spread(function (template, messageCount, editCount, firstMessage, randomMessage, emoticonCounts) {
+				.spread(function (template, messageCount, editCount, activeDays, firstMessage, randomMessage, emoticonCounts) {
+
+					var mostActiveDayObject = _.first(activeDays);
+					var mostActiveYear = mostActiveDayObject._id.year;
+					var mostActiveDayOfYear = mostActiveDayObject._id.dayOfYear;
+					var mostActiveDay = moment().year(mostActiveYear).dayOfYear(mostActiveDayOfYear);
+					var dateFormat = 'MMMM D, YYYY';
+					var dateTimeFormat = 'MMMM D, YYYY h:mma';
+
 					var data = {
 						user: roomMember.user.nick,
 						messageCount: messageCount,
 						editCount: editCount,
-						startDate: roomMember.user.createdAt,
+						startDate: moment(roomMember.user.createdAt).format(dateFormat),
+						activeDate: mostActiveDay.format(dateFormat) + ' (' + mostActiveDayObject.count + ' messages)',
 						totalDays: moment().diff(roomMember.user.createdAt, 'days'),
-						activeDays: 'WORK IN PROGRESS',
-						firstMessage: '"' + ent.decode(firstMessage[0].text) + '" (' + moment(firstMessage.createdAt).format() + ')',
+						activeDays: activeDays.length,
+						firstMessage: '"' + ent.decode(firstMessage[0].text) + '" (' + moment(firstMessage.createdAt).format(dateTimeFormat) + ')',
 						emotes: ent.decode(_.pluck(emoticonCounts, 'emoticon').join(' ')),
-						randomMessage: '"' + ent.decode(randomMessage[0].text) +'" (' +  moment(randomMessage.createdAt).format() + ')'
+						randomMessage: '"' + ent.decode(randomMessage[0].text) + '" (' + moment(randomMessage.createdAt).format(dateTimeFormat) + ')'
 					};
 					return ent.encode(_.template(template)(data));
 				});
 		});
 };
+
+function getActiveDays(roomMember) {
+	return new Promise(function (resolve) {
+		Message.native(function (err, messageCollection) {
+			messageCollection.aggregate([
+				{
+					$match: {
+						author: new ObjectId(roomMember.user.id)
+					}
+				},
+				{
+					$group: {
+						_id: {
+							year: {$year: '$createdAt'},
+							dayOfYear: {$dayOfYear: '$createdAt'}
+						},
+						count: {$sum: 1}
+					}
+				},
+				{
+					$sort: {count: -1}
+				}
+			], function (err, daysWithMessages) {
+				resolve(daysWithMessages);
+			});
+		});
+	});
+}
 
 function getEmoticonCounts(roomMember) {
 	var emoticonRegex = /:\w+:/g;
