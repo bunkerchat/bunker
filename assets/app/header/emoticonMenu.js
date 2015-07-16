46071c70d8eb210f379909d4930d3b5c3da92f7a
Jason More <Jason.More@gmail.com>
2015-07-16 12:34:32 -0500

    made emoticon menu collapse when clicking off

--- assets/app/header/emoticonMenu.js
+++ assets/app/header/emoticonMenu.js
@@ -6,6 +6,17 @@ app.directive('emoticonMenu', function ($rootScope, bunkerData, emoticons, fuzzy
 		},
 		controllerAs: 'menu',
 		bindToController: true,
+		link: function (scope, element) {
+			var elementsToSkip = 'li[ng-class="{active: header.emoticonMenu}"], [emoticon-menu]'
+
+			$(document)
+				.on('click.bunker.emoticon-menu', scope.menu.close)
+				.on('click.bunker.emoticon-menu', elementsToSkip, function (e) { e.stopPropagation() })
+
+			scope.$on('$destroy', function () {
+				$(document).off('click.bunker.emoticon-menu');
+			});
+		},
 		controller: function ($scope) {
 			var self = this;
 			this.search = '';
@@ -18,16 +29,18 @@ app.directive('emoticonMenu', function ($rootScope, bunkerData, emoticons, fuzzy
 			this.appendEmoticonToChat = function (emoticonName) {
 				var emote = ":" + emoticonName + ":";
 				$rootScope.$broadcast('inputText', emote);
-
-				// Close window
-				if (typeof self.visible !== 'undefined') {
-					self.visible = false;
-				}
 			};
 
 			this.sortPopularChanged = function () {
 				bunkerData.saveUserSettings();
 				prepareList();
+			};
+
+			this.close = function () {
+				// Close window
+				if (typeof self.visible !== 'undefined') {
+					self.visible = false;
+				}
 			};
 
 			function prepareList() {
