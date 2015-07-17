5d184e0c5f581f79be3e004c88c208408f98ebb1
Jason More <Jason.More@gmail.com>
2015-07-17 11:22:22 -0500

    pick random image

--- api/services/googleSearchService.js
+++ api/services/googleSearchService.js
@@ -35,13 +35,15 @@ module.exports.gifSearch = function (query) {
 		}
 	})
 		.spread(function (response, body) {
-			var image = _.filter(body.responseData.results, function (result) {
+			var images = _.filter(body.responseData.results, function (result) {
 				if(result.unescapedUrl.indexOf('giphy') > -1) return false;
 				if(result.unescapedUrl.indexOf('ytimg') > -1) return false;
 				if(result.unescapedUrl.indexOf('gifsec') > -1) return false;
 				return true;
 			});
 
-			if(image.length) return image[0].unescapedUrl;
+			if(images.length) {
+				return _.sample(images).unescapedUrl;
+			}
 		});
 };
