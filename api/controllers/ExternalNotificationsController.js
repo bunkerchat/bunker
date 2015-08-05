847703248ecf7c1f1de2e94cb0dc97e6298494d3
Jason More <Jason.More@gmail.com>
2015-08-05 11:15:10 -0500

    made build notifications persist through deploys

--- api/controllers/ExternalNotificationsController.js
+++ api/controllers/ExternalNotificationsController.js
@@ -1,9 +1,9 @@
-var lastStatus;
+var Promise = require('bluebird');
 
 module.exports.jenkinsBestBuy = function (req, res) {
-
 	var notification = req.body;
 	var build = notification.build;
+	var roomId;
 
 	// new jenkins isn't passing full_url anymore :-(
 	//if (build.full_url.indexOf('bestbuy.com') == 0) return res.ok('welp');
@@ -12,33 +12,40 @@ module.exports.jenkinsBestBuy = function (req, res) {
 	// only show completed builds
 	if (build.phase != 'COMPLETED') return res.ok('thanks');
 
-	// don't show successful builds if the last build was a success
-	if(lastStatus == 'SUCCESS' && build.status == 'SUCCESS') return res.ok('thanks2');
-
-	lastStatus = build.status;
-
-	var roomId;
-	var emote = build.status == 'FAILURE' ? ' :buildchicken:' : ':unsmith:';
-	var url = full_url + "console";
-	var protractorUrl = full_url + 'artifact/e2e_screenshots/my-report.html';
-	var text = emote + ' Build Notification: { name: "' + notification.name + '" , status: "' + build.status + '", link: ' + url + ', protractorReport: ' + protractorUrl + ' };';
-
-	Room.findOne({name: 'minos'})
-		.then(function (room) {
+	Promise.join(
+		Room.findOne({name: 'minos'}),
+		ExternalNotifications.findOne({type: notification.name})
+	)
+		.spread(function (room, externalNotification) {
 			roomId = room.id;
-			return Message.create({
-				room: room.id,
-				text: text,
-				type: 'buildNotification'
-			})
+			return externalNotification || ExternalNotifications.create({type: notification.name});
 		})
-		.then(function (message) {
-			return Message.findOne(message.id).populateAll();
-		})
-		.then(function (message) {
-			Room.message(roomId, message);
-			return message;
-		})
-		.then(res.ok)
-		.catch(res.serverError);
+		.then(function (externalNotification) {
+			// don't show successful builds if the last build was a success
+			if (externalNotification.lastStatus == 'SUCCESS' && build.status == 'SUCCESS') return res.ok('thanks2');
+			externalNotification.lastStatus = build.status;
+
+			return externalNotification.save()
+				.then(function () {
+					var emote = build.status == 'FAILURE' ? ' :buildchicken:' : ':unsmith:';
+					var url = full_url + "console";
+					var protractorUrl = full_url + 'artifact/e2e_screenshots/my-report.html';
+					var text = emote + ' Build Notification: { name: "' + notification.name + '" , status: "' + build.status + '", link: ' + url + ', protractorReport: ' + protractorUrl + ' };';
+
+					return Message.create({
+						room: roomId,
+						text: text,
+						type: 'buildNotification'
+					})
+				})
+				.then(function (message) {
+					return Message.findOne(message.id).populateAll();
+				})
+				.then(function (message) {
+					Room.message(roomId, message);
+					return message;
+				})
+				.then(res.ok)
+				.catch(res.serverError);
+		});
 };
---
+++ api/models/ExternalNotifications.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+	attributes: {
+		type: 'string',
+		lastStatus:  'string'
+	}
+};
