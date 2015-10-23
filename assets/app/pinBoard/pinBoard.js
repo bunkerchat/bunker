e61e1046bc9504a992bc4237cd8c65f237baeabf
DigitalZebra <andrew.scott.gerber@gmail.com>
2015-10-23 16:45:49 -0500

    more pinning progress

--- assets/app/pinBoard/pinBoard.js
+++ assets/app/pinBoard/pinBoard.js
@@ -20,4 +20,28 @@ app
 
 			}
 		};
-	}]);
+	}])
+	.directive('messagePin', ['pinBoard', function(pinBoard) {
+			return {
+				restrict: 'A',
+				templateUrl: '/assets/app/pinBoard/messagePin.html',
+				link: function(scope, element, attrs) {
+
+				}
+			};
+		}])
+	.directive('messagePinController', ['pinBoard', function(pinBoard) {
+			return {
+				restrict: 'A',
+				link: function(scope, element, attrs) {
+					element.on('click', '.message-pin-icon', function(evt) {
+
+						// TODO: fix this... need some way to delegate back to the actual pin
+						// and tell it to update.
+
+						debugger;
+
+					});
+				}
+			};
+		}]);
