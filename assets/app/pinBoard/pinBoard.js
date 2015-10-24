c650bca8efde0359b1db1d2237d09511075d03b4
DigitalZebra <DigitalZebra@users.noreply.github.com>
2015-10-24 00:21:45 -0500

    pinssss

--- assets/app/pinBoard/pinBoard.js
+++ assets/app/pinBoard/pinBoard.js
@@ -1,10 +1,14 @@
 app
-	.factory('pinBoard', ['$window', function($window) {
+	.factory('pinBoard', ['$window', '$rootScope', '$q', function($window, $rootScope, $q) {
 		var io = $window.io;
 
 		return {
-			savePin: function() {
-				// TODO: implement me!
+			savePin: function(messageId) {
+				return $q(function(resolve) {
+					io.socket.post('/room/' + $rootScope.roomId + '/pins', { messageId: messageId }, function() {
+						resolve();
+					});
+				});
 			}
 		};
 	}])
@@ -34,13 +38,22 @@ app
 			return {
 				restrict: 'A',
 				link: function(scope, element, attrs) {
-					element.on('click', '.message-pin-icon', function(evt) {
+					element.on('click', '.message-info', function(evt) {
+
+						// TODO: if pinned, un pin.
+						// also, populate pins with set from server.
+						var $target = $(this);
+
+						var messageId = $target.attr('message-pin-id');
+
+						pinBoard.savePin(messageId).then(function() {
+							// Mark dom as pinned!
+							$target.children(0).removeClass('fa-bookmark-o');
+							$target.children(0).addClass('fa-bookmark');
+						});
 
 						// TODO: fix this... need some way to delegate back to the actual pin
 						// and tell it to update.
-
-						debugger;
-
 					});
 				}
 			};
