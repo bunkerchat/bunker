c3c2e03778a5bb738509736dea49ac2ff4588713
Joshua Prodahl <jprodahl@gmail.com>
2015-11-09 14:04:16 -0600

    two can play at that game

--- assets/app/messages/notifications.js
+++ assets/app/messages/notifications.js
@@ -25,7 +25,7 @@ app.factory('notifications', function ($rootScope, bunkerData, $notification, $t
 		if (bunkerData.userSettings.playSoundOnMention) {
 			if (bunkerIsVisible || !bunkerData.mentionsUser(message.text)) return;
 
-			if (bunkerData.user.nick === 'jpro') {
+			if (bunkerData.user.nick === 'Jason') {
 				mentionSoundAlt.play();
 			}
 			else {
