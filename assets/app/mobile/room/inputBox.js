a3d1eb45463854468f750df46b8b5f740259f3b4
xjpro <jprodahl@gmail.com>
2015-12-08 15:02:47 -0600

    refactoring evil scroll hacks

--- assets/app/mobile/room/inputBox.js
+++ assets/app/mobile/room/inputBox.js
@@ -10,46 +10,5 @@ app.component('inputBox', {
 			bunkerData.createMessage(this.roomId, this.text);
 			delete this.text;
 		};
-
-		var focused = false;
-
-		$(window).scroll(_.debounce(function () {
-			// while scrolling
-			//$('input-box').hide();
-		}, 150, {'leading': true, 'trailing': false}));
-
-		$(window).scroll(_.debounce(function () {
-			// stopped scrolling
-			position();
-			$('input-box').show();
-		}, 150));
-
-		//attempt 5ish
-		$(document)
-			.on('focus', 'input', function () {
-				focused = true;
-				position();
-				setTimeout(function() { $(window).scroll(); }, 10);
-			})
-			.on('blur', 'input', function () {
-				focused = false;
-				unposition();
-			});
-
-		function position() {
-			if (!focused) return;
-			$('input-box').css({
-				position: 'absolute',
-				top: window.scrollY + window.innerHeight - 46 + 'px'
-			});
-		}
-
-		function unposition() {
-			$('input-box').css({
-				position: 'fixed',
-				top: 'initial',
-				bottom: '0'
-			});
-		}
 	}
 });
