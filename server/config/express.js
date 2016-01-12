12393bf0aeb5bfb97e7cb1036b0c1ff066e4a931
xjpro <jprodahl@gmail.com>
2016-01-12 17:31:59 -0600

    remove sass middleware

--- server/config/express.js
+++ server/config/express.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
-var sassMiddleware = require('node-sass-middleware');
 var auth = require('./auth');
 var config = require('./config');
 
