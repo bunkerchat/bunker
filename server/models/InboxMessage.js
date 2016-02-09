8287626f6e33793df4913d8d4749ed2a0ceddd23
Jason More <Jason.More@gmail.com>
2016-02-09 09:44:31 -0600

    fixed created at for inboxMessage

--- server/models/InboxMessage.js
+++ server/models/InboxMessage.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var inboxMessageSchema = new mongoose.Schema({
 	createdAt: {
 		type: Date,
-		default: new Date()
+		default: Date.now
 	},
 	user: {
 		type: mongoose.Schema.Types.ObjectId,
