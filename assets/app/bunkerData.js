8ca281a7ebb7b77752e5a11c25757c3f65c3636b
Jason More <Jason.More@gmail.com>
2016-03-30 14:50:04 -0500

    refactored gravatar

--- assets/app/bunkerData.js
+++ assets/app/bunkerData.js
@@ -1,4 +1,4 @@
-app.factory('bunkerData', function ($rootScope, $q, $window, $timeout, $notification, bunkerConstants, emoticons, $interval, pinBoard) {
+app.factory('bunkerData', function ($rootScope, $q, $window, $timeout, $notification, bunkerConstants, emoticons, $interval, pinBoard, gravatarService) {
 
 	var io = $window.io;
 	var roomLookup = []; // For fast room lookup
@@ -38,14 +38,14 @@ app.factory('bunkerData', function ($rootScope, $q, $window, $timeout, $notifica
 				_.assign(bunkerData.inbox, initialData.inbox);
 				_.assign(bunkerData.memberships, initialData.memberships);
 
+				_.each(initialData.users, user => {
+					user.$present = bunkerData.isPresent(user);
+					user.$gravatar = gravatarService.url(user.email, {s:20});
+				});
+
 				// instead of sending many duplicate users down, send one list
 				// then re-hydrate all the associations
-				users.length = 0;
-				_.assign(users, _.keyBy(initialData.users, '_id'));
-
-				_.each(users, user => {
-					user.$present = bunkerData.isPresent(user);
-				});
+				users = _.keyBy(initialData.users, '_id');
 
 				_.each(bunkerData.inbox, function (inbox) {
 					if (inbox.message.author) { // TODO causing a bug for @aSig for some reason without this
