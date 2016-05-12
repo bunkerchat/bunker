4c4a1d8dc7efa5ec0e62414935ee18b84fa7af22
Alexander Mann <alexandergmann@gmail.com>
2016-05-12 13:47:59 -0400

    Adding descriptins field to user, setinfo function, whois function

--- server/services/messageService.js
+++ server/services/messageService.js
@@ -77,6 +77,12 @@ messageService.createMessage = function (roomMember, text) {
 	else if (/^\/(promote|demote)\s+([\w\s\-\.]{0,19})/i.test(text)) {
 		return changeUserRole(roomMember, text);
 	}
+	else if (/^\/setInfo\s+/i.test(text)) {
+		return setInfo(roomMember, text);
+	}
+	else if (/^\/whois\s+/i.test(text)) {
+		return whois(roomMember, text);
+	}
 	else {
 		return message(roomMember, text, 'standard');
 	}
@@ -584,4 +590,39 @@ function leaderboard(roomMember, text) {
 				})
 				.then(broadcastMessage);
 		})
+}
+
+function setInfo(roomMember, text) {
+	var infoMatch = text.match(/\/setInfo\s+(.+)/i);
+	var info = infoMatch[1].substring(0, 50);
+	var user = roomMember.user;
+
+	return Promise.join(
+		User.findByIdAndUpdate(user._id, {description: info}, {new: true}),
+		RoomMember.find({user: user._id})
+		)
+		.spread(function (updatedUser, memberships) {
+			socketio.io.to('user_' + updatedUser._id)
+				.emit('user', {
+					_id: updatedUser._id,
+					verb: 'updated',
+					data: {description: updatedUser.description}
+				});
+			RoomService.messageRooms(_.map(memberships, 'room'), updatedUser.nick + ' updated their whois info');
+		});
+}
+
+function whois(roomMember, text) {
+	var nickMatches = text.match(/^\/whois\s+([\w\s\-\.]{0,19})/i);
+	var userNick = nickMatches[1];
+	var roomId = roomMember.room;
+
+	return RoomService.getRoomMemberByNickAndRoom(userNick, roomId)
+		.then(function (whoisUser) {
+			if (!whoisUser) throw new InvalidInputError('Could not find user ' + userNick);
+			var userEmail = whoisUser.user.email;
+			var userDescription = whoisUser.user.description;
+			var message = "Whois " + whoisUser.user.nick + ": " + userEmail + " -- " + userDescription;
+			RoomService.messageRoom(roomId, message);
+		});
 }
