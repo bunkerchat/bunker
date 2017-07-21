6efb253961493c84bebc85ac59a3b96e82fcbf35
Jason More <Jason.More@gmail.com>
2017-07-21 15:20:21 -0500

    rejiggered results to not make everything a png

--- server/services/imageSearch.js
+++ server/services/imageSearch.js
@@ -8,21 +8,19 @@ var imageSearch = module.exports;
 
 imageSearch.image = function (query) {
 	return googleImageSearch(query)
-		// .catch(() => bingImageSearch(query, true))
-		.catch(() => ({provider: 'none', images:[]}));
+		.catch(() => ({provider: 'none', images: []}));
 };
 
 imageSearch.gif = function (query) {
 	return googleImageSearch(query, true)
-		// .catch(() => bingImageSearch(query, true))
-		.catch(() => ({provider: 'none', images:[]}));
+		.catch(() => ({provider: 'none', images: []}));
 };
 
 function googleImageSearch(query, animated) {
 	var key = `imageSearch/google-${animated ? 'gif' : 'image'}|${query}`;
-	return ImageCache.findOne({key:key})
+	return ImageCache.findOne({key: key})
 		.then(dbCache => {
-			if(dbCache) {
+			if (dbCache) {
 				log.info(`cache hit: ${key}`)
 				return dbCache.results
 			}
@@ -30,7 +28,7 @@ function googleImageSearch(query, animated) {
 		})
 
 	function lookup() {
-		if(!query) return Promise.reject('no query');
+		if (!query) return Promise.reject('no query');
 
 		var qs = {
 			q: query,
@@ -65,81 +63,26 @@ function googleImageSearch(query, animated) {
 				}
 			})
 			.then(results => {
-				if(results) log.info(`no cache: ${key}`)
+				if (results) log.info(`no cache: ${key}`)
 				return ImageCache.create({key, results})
 			})
-			.then(dbCache =>{
+			.then(dbCache => {
 				return dbCache.results
 			})
 	}
 }
 
-var encodedBingKey = new Buffer(config.bingApiKey + ":" + config.bingApiKey).toString("base64");
-
-function bingImageSearch(query, animated) {
-	var key = `imageSearch/bing-${animated ? 'gif' : 'image'}|${query}`;
-	return cacheService.fourMonths.getAsync(key)
-		.then(result => result || lookup())
-		.then(JSON.parse);
-
-	function lookup() {
-		if(!query) return Promise.reject('no query');
-
-		var qs = {
-			$format: 'json',
-			Query: `'${query}'`,
-			Adult: "'Strict'"
-		};
-
-		if (animated) {
-			// arg why?! no good api unfortunately
-			qs.Query = `'${query} ".gif"'`;
-		}
-
-		return request.getAsync({
-			json: true,
-			url: "https://api.datamarket.azure.com/Bing/Search/Image",
-			headers: {
-				"Authorization": `Basic ${encodedBingKey}`
-			},
-			qs: qs
-		})
-			.spread((res, body) => {
-				if (res.statusCode === 403) {
-					console.log('sad face');
-					return Promise.reject(new Error('over capacity'));
-				}
-
-				if (res.statusCode == 400) {
-					console.error('bing error', body)
-					return Promise.reject(body);
-				}
-
-				// we don't need 50 results
-				body.d.results.length = 12;
-
-				return {
-					provider: 'bing',
-					images: _(body.d.results).map('MediaUrl').map(ensureResult).value()
-				}
-			})
-			.then(JSON.stringify)
-			.then(results => cacheService.fourMonths.setAsync(key, results));
-	}
-}
-
-function ensureResult(url, animated) {
-	if (animated === true) {
-		return ensureImageExtension(url.replace(/(giphy\.com\/.*)\/.+_s.gif$/, '$1/giphy.gif'));
-	} else {
-		return ensureImageExtension(url);
-	}
+function ensureResult(url) {
+	url = url.replace(/(giphy\.com\/.*)\/.+_s.gif$/, '$1/giphy.gif')
+	return ensureImageExtension(url);
 }
 
 function ensureImageExtension(url) {
-	if (/(png|jpe?g|gif)$/i.test(url)) {
-		return url;
+	// check if it doesn't end in file extension
+	const match = /(png|jpe?g|gif).+$/i.exec(url)
+	if (match && match[1]) {
+		return url + `#.${match[1]}`
 	} else {
-		return url + "#.png";
+		return url;
 	}
 }
