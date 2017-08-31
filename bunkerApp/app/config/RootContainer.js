79865fec2c3ef33faee5bb37512b988f3ac039a4
Jason More <Jason.More@gmail.com>
2017-08-31 10:44:17 -0500

    got my google user

--- bunkerApp/app/config/RootContainer.js
+++ bunkerApp/app/config/RootContainer.js
@@ -1,27 +1,57 @@
 import React from 'react'
 import {StatusBar, StyleSheet, View, Text} from 'react-native'
+import {GoogleSignin, GoogleSigninButton} from 'react-native-google-signin'
 import {connect} from 'react-redux'
 import AppNavigator from './AppNavigator'
+import {login} from '../user/userReducer'
 
 class RootContainer extends React.PureComponent {
-	render(){
+
+	componentDidMount() {
+		this._setupGoogleSignin()
+	}
+
+	async _setupGoogleSignin() {
+		try {
+			await GoogleSignin.hasPlayServices({ autoResolve: true });
+			await GoogleSignin.configure({
+				iosClientId: '744915257573-1ajg3qgmn4k261i0vdq6ed042c6sba1a.apps.googleusercontent.com',
+				webClientId: '744915257573-ri8suktjsu5s1b3jddkacm6k0a45vi02.apps.googleusercontent.com',
+				offlineAccess: false
+			});
+
+			const {loggedInUser, login} = this.props
+			if(loggedInUser) return
+
+			const user = await GoogleSignin.currentUserAsync();
+			console.log(user);
+			login(user)
+		}
+		catch(err) {
+			console.log("Google signin error", err.code, err.message);
+		}
+	}
+
+	render() {
 		return <View style={styles.applicationView}>
-			<StatusBar  />
-			<AppNavigator />
+			<StatusBar/>
+			<AppNavigator/>
 		</View>
 	}
 }
 
 const styles = StyleSheet.create({
-	applicationView:{
+	applicationView: {
 		flex: 1
 	}
 })
 
 const mapStateToProps = (state, props) => {
-	return {}
+	return {
+		loggedInUser: state.user.loggedInUser
+	}
 }
 
-const actions = {}
+const actions = {login}
 
 export default connect(mapStateToProps, actions)(RootContainer)
--- bunkerApp/app/user/LoginScreen.js
+++ bunkerApp/app/user/LoginScreen.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {StatusBar, StyleSheet, View, Text, Button} from 'react-native'
+import {GoogleSignin, GoogleSigninButton} from 'react-native-google-signin';
 import {connect} from 'react-redux'
 import {login} from './userReducer'
 
@@ -14,17 +15,48 @@ class LoginScreen extends React.PureComponent{
 	}
 
 	_renderLoginButton(){
-		const {login} = this.props
+		return <GoogleSigninButton
+			style={{width: 48, height: 48}}
+			size={GoogleSigninButton.Size.Icon}
+			color={GoogleSigninButton.Color.Dark}
+			onPress={this._signIn.bind(this)}/>
+	}
 
-		const fakeUser = {
-			username: 'jmore',
-			email: 'derp@derp.com'
+	async _setupGoogleSignin() {
+		try {
+			await GoogleSignin.hasPlayServices({ autoResolve: true });
+			await GoogleSignin.configure({
+				iosClientId: '603421766430-mjg34tcspqcio7eld8hu4djv5vjdvtsr.apps.googleusercontent.com',
+				webClientId: '603421766430-60og8n04mebic8hi49u1mrcmcdmugnd5.apps.googleusercontent.com',
+				offlineAccess: false
+			});
+
+			const user = await GoogleSignin.currentUserAsync();
+			console.log(user);
+			this.setState({user});
 		}
+		catch(err) {
+			console.log("Google signin error", err.code, err.message);
+		}
+	}
 
-		return <View>
-			<Text> Click Login </Text>
-			<Button onPress={() => login(fakeUser)} title="Login" />
-		</View>
+	_signIn() {
+		GoogleSignin.signIn()
+			.then((user) => {
+				console.log(user);
+				this.setState({user: user});
+			})
+			.catch((err) => {
+				console.log('WRONG SIGNIN', err);
+			})
+			.done();
+	}
+
+	_signOut() {
+		GoogleSignin.revokeAccess().then(() => GoogleSignin.signOut()).then(() => {
+			this.setState({user: null});
+		})
+			.done();
 	}
 
 	render(){
