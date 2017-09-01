579dd1b993ac18eb02a5dc8dffda426dc114550f
Jason More <Jason.More@gmail.com>
2017-09-01 07:42:09 -0500

    set users

--- bunkerApp/app/user/userReducer.js
+++ bunkerApp/app/user/userReducer.js
@@ -1,9 +1,11 @@
+import _ from 'lodash'
 import Immutable from 'seamless-immutable'
 import {makeReducer} from '../config/reduxTools'
 
 const INITIAL_STATE = Immutable({
 	loggedInUser: null,
-	userSettings: null
+	userSettings: null,
+	users:null
 })
 
 const reducer = {}
@@ -11,6 +13,8 @@ const reducer = {}
 export const login = loggedInUser => ({type: 'user/login', loggedInUser})
 reducer['user/login'] = (state, {loggedInUser}) => state.merge({loggedInUser})
 
-reducer['socketio-init'] = (state, {userSettings}) => state.merge({userSettings})
+reducer['socketio-init'] = (state, {userSettings, users}) => {
+	return state.merge({userSettings, users: _.keyBy(users, '_id')})
+}
 
 export default makeReducer(INITIAL_STATE, reducer)
