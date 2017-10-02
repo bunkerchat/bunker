d41cb56cdf0aedb91cfc0c3ea4835961bf65a0ad
Adam Morris <adam@kernelpanicstudios.com>
2017-10-02 15:56:55 -0500

    Fixed escape characters in bunker. Maybe. (#393)

--- assets/app/messages/bunkerMessage.js
+++ assets/app/messages/bunkerMessage.js
@@ -180,6 +180,19 @@ app.directive('bunkerMessage', function ($sce, $compile, emoticons, bunkerData) 
 
 					var match;
 					while ((match = lookup.exec(text)) !== null) {
+						var charBefore = match.index - 1;
+						if (text[charBefore] && text[charBefore] === '\\') {
+							var startIndex = match.index - 1 > 0 ? match.index - 1 : 0;
+							var newText = '';
+							if (startIndex > 0) {
+								newText += text.slice(0, startIndex);
+							}
+							var cleanMatch = match[0].substr(0, match[0].lastIndexOf('\\')) + match[0].substr(match[0].lastIndexOf('\\') + 1);
+							newText += cleanMatch;
+							newText += text.slice(match.index + match[0].length);
+							text = newText;
+							continue;
+						}
 						text = replaceAll(text, match[0].trim(), '<' + type.tag + '>' + replaceAll(match[0], type.marker, '') + '</' + type.tag + '>');
 					}
 				});
