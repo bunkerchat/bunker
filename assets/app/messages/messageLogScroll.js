07b606a30fb9e4de977242e90604aea941c1654f
Jason More <Jason.More@gmail.com>
2017-12-15 10:32:40 -0600

    smooth scroll chat

--- assets/app/messages/messageLogScroll.js
+++ assets/app/messages/messageLogScroll.js
@@ -78,7 +78,7 @@ app.directive('messageLogScroll', function ($timeout, $rootScope, bunkerData, an
 
 			function scroll(waitTime) {
 				$timeout(() => {
-					elem.scrollTop(el.scrollHeight);
+					elem.animate({ scrollTop: el.scrollHeight });
 				}, waitTime || 0);
 			}
 		}
