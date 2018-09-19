f439077d1d77c0413cb49ce0dde3f9d8a9f26713
Joshua Prodahl <jprodahl@gmail.com>
2018-09-19 10:32:00 -0500

    header links

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 export default class Header extends React.Component {
 	render() {
 		return (
 			<div>
 				<nav className="navbar navbar-expand-lg navbar-light bg-light">
-					<a className="navbar-brand" href="#">Bunker</a>
-						<div className="navbar-nav">
-							<a className="nav-item nav-link" href="#">Room 1</a>
-							<a className="nav-item nav-link" href="#">Room 2</a>
-							<a className="nav-item nav-link" href="#">Room 3</a>
-						</div>
+					<Link className="navbar-brand" to={`/2/lobby`}>Bunker</Link>
+					<div className="navbar-nav">
+						<Link className="nav-item nav-link" to={`/2/room/1`}>Room 1</Link>
+						<Link className="nav-item nav-link" to={`/2/room/2`}>Room 2</Link>
+						<Link className="nav-item nav-link" to={`/2/room/3`}>Room 3</Link>
+					</div>
 				</nav>
 			</div>
 		)
