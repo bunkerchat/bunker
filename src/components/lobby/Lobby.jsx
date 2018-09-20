272e06ab3548753476239d25e0d146b3d8e3f414
Joshua Prodahl <jprodahl@gmail.com>
2018-09-20 16:28:13 -0500

    connected lobby

--- src/components/lobby/Lobby.jsx
+++ src/components/lobby/Lobby.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 
-export default class Lobby extends React.Component {
+const mapStateToProps = state => ({
+	rooms: state.rooms
+});
+
+class Lobby extends React.Component {
 	render() {
+		const {rooms} = this.props;
 		return (
-			<div>
-				this would be the lobby, you should implement this tia
+			<div className="container-fluid">
+				{_.map(rooms, (room, roomId) => (
+					<div key={roomId}>
+						<Link to={`/2/room/${roomId}`}>
+							{room.name}
+						</Link>
+					</div>
+				))}
 			</div>
-		)
+		);
 	}
 }
+
+export default connect(mapStateToProps)(Lobby);
