dff8fc55f24432237641229c43e69786ce374aa7
Joshua Prodahl <jprodahl@gmail.com>
2018-09-20 10:39:01 -0500

    some nonsnese to get a deploy to happen

--- src/components/lobby/Lobby.jsx
+++ src/components/lobby/Lobby.jsx
@@ -4,7 +4,7 @@ export default class Lobby extends React.Component {
 	render() {
 		return (
 			<div>
-				this would be the lobby, you should implement this
+				this would be the lobby, you should implement this tia
 			</div>
 		)
 	}
