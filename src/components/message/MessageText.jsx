0b7e623f6cffcdc27c417172f1b46bd2daab7bdd
Joshua Prodahl <jprodahl@gmail.com>
2018-09-26 14:13:16 -0500

    standardize emoticons

--- src/components/message/MessageText.jsx
+++ src/components/message/MessageText.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import parseEmoticons from "./parsers/parseEmoticons";
 import parseMedia from "./parsers/parseMedia";
 import parseFormatting from "./parsers/parseFormatting";
+import styled from "styled-components"
+
+const MessageTextContainer = styled.div`
+	.emoticon {
+		height: 24px;
+	}
+`;
 
 // Note I felt it was okay to use dangerouslySetInnerHTML since we escape everything on the server
 // todo could be improved?
@@ -10,7 +17,7 @@ export default class MessageText extends React.Component {
 		let { text } = this.props;
 		text = parseText(text);
 
-		return <div dangerouslySetInnerHTML={{ __html: text }} />;
+		return <MessageTextContainer dangerouslySetInnerHTML={{ __html: text }} />;
 	}
 }
 
