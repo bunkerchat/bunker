267018e962d30747263c4a5cdbf1f0fdabbe2d6b
Joshua Prodahl <jprodahl@gmail.com>
2018-10-02 15:20:31 -0500

    comment about what touch scrolling is

--- src/features/message/ScrollingMessageList.jsx
+++ src/features/message/ScrollingMessageList.jsx
@@ -11,11 +11,13 @@ const MessageListContainer = styled.div`
 	flex: 1;
 	overflow-y: scroll;
 	overflow-x: hidden;
-	-webkit-overflow-scrolling: touch;
-	z-index: 0; // apparently makes android support touch overflow scrolling?
-
+	
 	// todo this hack makes the scrolling box honor our overflow: scroll, not sure why this is needed and I'm sad it's here
 	height: calc(100vh - ${theme.inputBox}px - ${theme.top}px);
+	
+	// these enable touch scrolling on iOS / Android 
+	-webkit-overflow-scrolling: touch;
+	z-index: 0;
 `;
 
 const mapStateToProps = (state, ownProps) => {
