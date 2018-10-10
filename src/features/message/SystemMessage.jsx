e4919583cabef5c5a8d2322e306671e7651d510a
Joshua Prodahl <jprodahl@gmail.com>
2018-10-10 17:26:17 -0500

    message text in system message

--- src/features/message/SystemMessage.jsx
+++ src/features/message/SystemMessage.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import MessageText from "./MessageText.jsx";
 
 export default class SystemMessage extends React.PureComponent {
 	render() {
 		const { message } = this.props;
-		return <div className="p-2">{message.text}</div>;
+		return (
+			<div className="p-2">
+				<MessageText text={message.text} />
+			</div>
+		);
 	}
 }
