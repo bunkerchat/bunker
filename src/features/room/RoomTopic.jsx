8da679c84eed4685c29a7dac9fc2c5fcd6aa472a
Joshua Prodahl <jprodahl@gmail.com>
2018-10-11 09:13:33 -0500

    no room topic on mobile

--- src/features/room/RoomTopic.jsx
+++ src/features/room/RoomTopic.jsx
@@ -10,7 +10,7 @@ class RoomTopic extends React.PureComponent {
 	render() {
 		const { topic } = this.props;
 		return topic ? (
-			<div className="p-2 border-dark border-top-0 border-bottom border-left-0 border-right-0">
+			<div className="p-2 border-dark border-top-0 border-bottom border-left-0 border-right-0 d-none d-md-block">
 				<MessageText className="p-2" text={topic} />
 			</div>
 		) : null;
