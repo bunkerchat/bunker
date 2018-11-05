22bfb724971313ab072d1c7e6dba4cb43086efa6
Jason More <Jason.More@target.com>
2018-11-05 09:09:15 -0600

    wip lexer

--- server/services/__tests__/tokenService.test.js
+++ server/services/__tests__/tokenService.test.js
@@ -2,9 +2,18 @@ const tokenService = require("../tokenService");
 
 describe("tokenService", () => {
 	describe("tokenize", () => {
-		it("derp", () => {
-			const tokens = tokenService.tokenize("asdf");
-			expect(tokens).toEqual([]);
+		it("sentence", () => {
+			const tokens = tokenService.tokenize("The Quick Brown Fox.");
+			expect(tokens).toMatch([
+				{ text: "The", type: "word" },
+				{ text: " ", type: "space" },
+				{ text: "Quick", type: "word" },
+				{ text: " ", type: "space" },
+				{ text: "Brown", type: "word" },
+				{ text: " ", type: "space" },
+				{ text: "Fox", type: "word" },
+				{ text: ".", type: "unknown" }
+			]);
 		});
 	});
 });
--- server/services/tokenService.js
+++ server/services/tokenService.js
@@ -1,7 +1,46 @@
 const tokenService = module.exports;
+const Lexer = require("flex-js");
+// const lexer = new Lexer();
 
-const Promise = require("bluebird");
+// // options
+// // lexer.setIgnoreCase(true);
+// //
+// // // definitions
+// // lexer.addDefinition("DIGIT", /[0-9]/);
+// //
+// // // rules
+// // lexer.addRule(/{DIGIT}+\.{DIGIT}+/, function(lexer) {
+// // 	console.log("Found float: " + lexer.text);
+// // });
+// // lexer.addRule(/\s+/);
+
+// lexer.setSource('abcd');
+// lexer.lex();
 
 tokenService.tokenize = text => {
-	return [];
+	// lexer.setSource(text);
+	// lexer.lex();
+
+	const output = [];
+	const lexer = new Lexer();
+
+	// word
+	lexer.addRule(/\w+/, lexer => {
+		output.push({ type: "word", text: lexer.text });
+	});
+
+	// white space
+	lexer.addRule(/\s+/, lexer => {
+		output.push({ type: "space", text: lexer.text });
+	});
+
+	// catch all
+	lexer.addRule(/./, lexer => {
+		output.push({ type: "unknown", text: lexer.text });
+	});
+
+	lexer.setSource(text);
+	lexer.lex();
+
+	return output;
 };
