33ffea262eb51ef0c125c75521edbe756d722446
Jason More <Jason.More@target.com>
2018-11-21 08:11:55 -0600

    removed background cause no good on dark themes

--- src/features/message/tokens/Quote.jsx
+++ src/features/message/tokens/Quote.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import styled from "styled-components";
 
 const Pre = styled.pre`
 	white-space: pre-wrap;
 `;
 
 const Quote = ({ token }) => (
-	<Pre className="p-2 bg-light text-dark border border-dark" dangerouslySetInnerHTML={{ __html: token.value }} />
+	<Pre className="p-2 border border-dark" dangerouslySetInnerHTML={{ __html: token.value }} />
 );
 
 export default Quote;
