96c1af98a11cc889ed810e7850eb1d4a868957d6
Jason More <Jason.More@target.com>
2018-11-21 10:45:47 -0600

    fixin things

--- server/services/tokenService.js
+++ server/services/tokenService.js
@@ -33,8 +33,8 @@ tokenService.tokenize = textToTokenize => {
 	lexerInstance.setIgnoreCase(true);
 
 	lexerInstance.addDefinition("ALPHA_NUM", /[a-z0-9]/);
-	lexerInstance.addDefinition("ALPHA_NUM_CHAR", /[a-z0-9,.'"!@#$%^&*()\-+=]/);
-	lexerInstance.addDefinition("ALPHA_NUM_CHAR_SPACE", /[a-z0-9,.'"!@#$%^&*()\-+=\s]/);
+	lexerInstance.addDefinition("ALPHA_NUM_CHAR", /[a-z0-9,.'"!@#$%^&()\-+=]/);
+	lexerInstance.addDefinition("ALPHA_NUM_CHAR_SPACE", /[a-z0-9,.'"!@#$%^&()\-+=\s]/);
 
 
 	// contains at least one line break
