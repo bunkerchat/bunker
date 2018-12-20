b37bafb3502cd9e71517d8bf77830b9da34dc560
djbielejeski <djbielejeski@gmail.com>
2018-12-20 10:07:26 -0600

    PascalCase

--- server/config/auth.js
+++ server/config/auth.js
@@ -1,7 +1,7 @@
 var Session = require("express-session");
 var MongoStore = require("connect-mongo")(Session);
 var passport = require("passport");
-var googleStrategy = require("passport-google-oauth20").Strategy;
+var GoogleStrategy = require("passport-google-oauth20").Strategy;
 var LocalStrategy = require("passport-local").Strategy;
 
 var config = require("./config");
@@ -36,7 +36,7 @@ auth.init = function(app) {
 	});
 
 	passport.use(
-		new googleStrategy(
+		new GoogleStrategy(
 			{
 				clientID: config.google.clientID,
 				clientSecret: config.google.clientSecret,
