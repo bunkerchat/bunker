63f10fd6ed117bd327f9bc46d0e788e773e64652
Joshua Prodahl <jprodahl@gmail.com>
2019-05-03 11:00:37 -0500

    url preview in v2

--- src/features/message/tokens/Url.jsx
+++ src/features/message/tokens/Url.jsx
@@ -1,12 +1,29 @@
 import React from "react";
+import ToggleButton from "./ToggleButton.jsx";
+import { toggleMessageImagesVisible } from "../messageActions";
+import {connect} from "react-redux";
 
-const Url = ({ value }) => {
+const Url = ({ value, message, toggleMessageImagesVisible}) => {
+	const toggleImagesVisible = event => {
+		event.stopPropagation();
+		toggleMessageImagesVisible(message);
+	};
+
 	const target = _.includes(value, window.location.origin) ? "_self" : "_blank";
 	return (
-		<a href={value} target={target}>
-			{value}
-		</a>
+		<>
+			<a href={value} target={target}>
+				{value}
+			</a>
+			{message.linkMeta && message.linkMeta.image && (
+				<ToggleButton toggled={message.imagesVisible} onToggle={toggleImagesVisible}/>
+			)}
+		</>
 	);
 };
 
-export default Url;
+const mapDispatchToProps = {
+	toggleMessageImagesVisible
+};
+
+export default connect(null, mapDispatchToProps)(Url);
