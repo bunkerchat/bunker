e92d6fadfa8f968f8d238a29a1c43df55da80aa9
Joshua Prodahl <jprodahl@gmail.com>
2019-05-07 13:39:16 -0500

    font-size 16 on inputs so ios doesn't zoom?

--- src/features/chat/Chat.jsx
+++ src/features/chat/Chat.jsx
@@ -16,6 +16,12 @@ const Container = styled.div`
 	flex-direction: column;
 	width: 100vw;
 	height: 100vh;
+	
+	input[type="text"],
+	select:focus,
+	textarea {
+		font-size: 16px;
+	}
 `;
 
 const mapStateToProps = state => ({
