3274153bb313cdd7f3af68fe835d9d6e2bad4191
Jason More <Jason.More@target.com>
2019-05-20 15:50:19 -0500

    chunks

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,35 @@
+const path = require('path');
+
 module.exports = {
 	devtool: "source-map",
 	entry: './src/index.jsx',
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	optimization: {
+		splitChunks: {
+			chunks: 'all',
+			minSize: 30000,
+			maxSize: 0,
+			minChunks: 1,
+			maxAsyncRequests: 5,
+			maxInitialRequests: 3,
+			automaticNameDelimiter: '~',
+			name: true,
+			cacheGroups: {
+				vendors: {
+					test: /[\\/]node_modules[\\/]/,
+					priority: -10
+				},
+				default: {
+					minChunks: 2,
+					priority: -20,
+					reuseExistingChunk: true
+				}
+			}
+		}
+	},
 	module: {
 		rules: [
 			{
