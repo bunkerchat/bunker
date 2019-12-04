f56d6179b92c8564e212dac1da09f2cea66b7403
Jason More <Jason.More@target.com>
2019-12-04 08:47:09 -0600

    cleanup

--- src/store.js
+++ src/store.js
@@ -1,7 +1,6 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { createBrowserHistory } from "history";
 import { connectRouter, routerMiddleware } from "connected-react-router";
-// import thunk from "redux-thunk";
 import rootReducer from "./rootReducer";
 
 const history = createBrowserHistory();
@@ -11,17 +10,6 @@ const store = configureStore({
 	middleware: [routerMiddleware(history), ... getDefaultMiddleware()]
 });
 
-//
-// const store = createStore(
-// 	connectRouter(history)(rootReducer), // new root reducer with router state
-// 	composeEnhancers(
-// 		applyMiddleware(
-// 			routerMiddleware(history), // for dispatching history actions
-// 			thunk
-// 			// ... other middlewares ...
-// 		)
-// 	)
-// );
 
 if (process.env.NODE_ENV === "development" && module.hot) {
 	module.hot.accept("./rootReducer", () => {
