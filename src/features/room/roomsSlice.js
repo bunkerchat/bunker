88fbd8349ff846770fdaf265207d4c28ab252621
Jason More <Jason.More@target.com>
2019-12-06 12:17:23 -0600

    cleanup

--- src/features/room/roomsSlice.js
+++ src/features/room/roomsSlice.js
@@ -1,6 +1,70 @@
 import { emit } from "../../api";
 import { getActiveRoomId } from "../../selectors/selectors";
 
+/* actions */
+
+
+export default function(state = {}, action) {
+	return handlers[action.type] ? handlers[action.type](state, action) : state;
+}
+
+export function roomUpdated(room) {
+	return { type: "room/updated", room };
+}
+
+export function loadingMessages(roomId) {
+	return { type: "message/loadingMany", roomId };
+}
+
+export function messageSent() {
+	return { type: "message/sent" };
+}
+
+export function messageReceived(message) {
+	return { type: "message/received", message };
+}
+
+export function messageHistoryReceived(roomId, messages) {
+	return { type: "message/receivedHistory", roomId, messages };
+}
+
+export function messageUpdated(message) {
+	return { type: "message/updated", message };
+}
+
+export function clearRoomMessages(roomId) {
+	return { type: "message/clear", roomId };
+}
+
+export function sendRoomMessage(roomId, text) {
+	return dispatch => {
+		return emit("/room/message", { roomId, text }).then(() => {
+			dispatch(messageSent());
+		});
+	};
+}
+
+export function loadRoomMessages(roomId, skip) {
+	return dispatch => {
+		dispatch(loadingMessages(roomId));
+		return emit("/room/messages", { roomId, skip: skip || 0 }).then(messages =>
+			dispatch(messageHistoryReceived(roomId, messages))
+		);
+	};
+}
+
+export const sendTypingNotification = () => (dispatch, getState) => {
+	const state = getState();
+	const activeRoomId = getActiveRoomId(state);
+	return emit("/user/current/typing", { typingIn: activeRoomId });
+};
+export const joinedRoom = room => ({ type: "room/joined", room });
+export const joinRoom = roomId => dispatch => {
+	return emit("/room/join", roomId).then(room => dispatch(joinedRoom(room)));
+};
+
+
+/* reducer */
 const setCurrentRoom = rooms => {
 	_.each(rooms, room => {
 		room.current = false;
@@ -60,63 +124,4 @@ const handlers = {
 			...action.room
 		}
 	})
-};
-
-export default function(state = {}, action) {
-	return handlers[action.type] ? handlers[action.type](state, action) : state;
-}
-
-export function roomUpdated(room) {
-	return { type: "room/updated", room };
-}
-
-export function loadingMessages(roomId) {
-	return { type: "message/loadingMany", roomId };
-}
-
-export function messageSent() {
-	return { type: "message/sent" };
-}
-
-export function messageReceived(message) {
-	return { type: "message/received", message };
-}
-
-export function messageHistoryReceived(roomId, messages) {
-	return { type: "message/receivedHistory", roomId, messages };
-}
-
-export function messageUpdated(message) {
-	return { type: "message/updated", message };
-}
-
-export function clearRoomMessages(roomId) {
-	return { type: "message/clear", roomId };
-}
-
-export function sendRoomMessage(roomId, text) {
-	return dispatch => {
-		return emit("/room/message", { roomId, text }).then(() => {
-			dispatch(messageSent());
-		});
-	};
-}
-
-export function loadRoomMessages(roomId, skip) {
-	return dispatch => {
-		dispatch(loadingMessages(roomId));
-		return emit("/room/messages", { roomId, skip: skip || 0 }).then(messages =>
-			dispatch(messageHistoryReceived(roomId, messages))
-		);
-	};
-}
-
-export const sendTypingNotification = () => (dispatch, getState) => {
-	const state = getState();
-	const activeRoomId = getActiveRoomId(state);
-	return emit("/user/current/typing", { typingIn: activeRoomId });
-};
-export const joinedRoom = room => ({ type: "room/joined", room });
-export const joinRoom = roomId => dispatch => {
-	return emit("/room/join", roomId).then(room => dispatch(joinedRoom(room)));
 };
