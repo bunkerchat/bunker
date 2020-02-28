bf16721519e8e4b4b36b57289558b0b25f7636b6
Jason More <Jason.More@target.com>
2020-02-28 12:19:08 -0600

    cleanup

--- src/features/users/localUserActions.js
+++ src/features/users/localUserActions.js
@@ -1,10 +1,6 @@
 import { emit } from "../../api";
 import { setActiveRoom } from "../room/roomSlice.js";
 
-// export function activeRoomChanged(roomId) {
-// 	return { type: "localUser/activeRoom", roomId };
-// }
-
 export function presentChanged(present) {
 	return { type: "localUser/present", present };
 }
