47ee0c91954a92d266bec54f020e52e235e4f541
Jason More <Jason.More@target.com>
2020-02-28 12:18:33 -0600

    cleanup

--- src/features/room/roomSlice.js
+++ src/features/room/roomSlice.js
@@ -3,13 +3,6 @@ import { createSlice } from "@reduxjs/toolkit";
 const roomSlice = createSlice({
 	name: "room",
 	initialState: { activeRoomId: null },
-	// extraReducers: {
-	// 	"@@router/LOCATION_CHANGE": (state, { payload }) => {
-	// 		// empty array is lobby
-	// 		const roomMatch = /room\/(\w+)/i.exec(payload.location.pathname) || [];
-	// 		state.activeRoomId = roomMatch[1];
-	// 	}
-	// },
 	reducers: {
 		setActiveRoom: (state, { payload }) => {
 			state.activeRoomId = payload;
