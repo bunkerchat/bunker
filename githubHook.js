cfd15a70961fac4bcbc963ea6cfd69afd854bf40
Jason More <Jason.More@target.com>
2020-03-11 13:52:31 -0500

    nvm use didnt work weird

--- githubHook.js
+++ githubHook.js
@@ -1,2 +1,2 @@
 var hookshot = require('hookshot');
-hookshot('refs/heads/master', 'git pull && nvm use && yarn deploy').listen(3333)
+hookshot('refs/heads/master', 'git pull && yarn deploy').listen(3333)
