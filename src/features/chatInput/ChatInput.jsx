a3a998fa415a8a4912d1508ce5e49900f2c60153
Jason More <Jason.More@target.com>
2020-03-17 13:47:14 -0500

    unbreak chat input on ios

--- src/features/chatInput/ChatInput.jsx
+++ src/features/chatInput/ChatInput.jsx
@@ -260,16 +260,16 @@ export function ChatInput({
 		if (emoticonPickerVisible && selectedEmoticon) {
 			event.preventDefault();
 			emoticonPicked(selectedEmoticon);
+			hideEmoticonPicker();
 		} else if (nickPickerVisible && selectedUserWithNick) {
 			event.preventDefault();
 			nickPicked(selectedUserWithNick);
+			hideNickPicker();
 		} else {
 			if (!isMobile) {
 				event.preventDefault();
 			}
 			onSend();
-			hideEmoticonPicker();
-			hideNickPicker();
 		}
 	}
 
