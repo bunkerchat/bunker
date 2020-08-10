d6a077d6e0f8c3e804736911e107634253bf2656
jpro <jprodahl@gmail.com>
2020-08-10 09:27:53 -0500

    only play in "Music" room

--- src/features/music/musicActions.js
+++ src/features/music/musicActions.js
@@ -2,7 +2,8 @@ export const playMusic = message => (dispatch, getState) => {
 	const state = getState();
 	if (message.type === "music") {
 		const userSettings = state.userSettings;
-		if (userSettings.playMusic) {
+		const room = state.rooms[message.room];
+		if (userSettings.playMusic && room.name === "Music") {
 			const uri = _.last(message.tokens).value;
 			window.location = `spotify:track:${uri}:play`;
 		}
